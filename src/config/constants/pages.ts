ae016bed6bba3cb3091efcf45f8731d9f284333e
mogw <mogwinfo@gmail.com>
2024-09-04 04:54:02 +0100

    Added pages constants

---
+++ src/config/constants/pages.ts
@@ -0,0 +1,32 @@
+import { PAGE } from "../../utils/type";
+
+export const PAGES: PAGE[] = [
+  {
+    name: "Vaults",
+    path: "/vaults",
+  },
+  {
+    name: "NFT",
+    path: "/nft/inventory",
+  },
+  {
+    name: "Real World Assets",
+    path: "/rwa",
+  },
+  {
+    name: "DAO",
+    path: "/dao",
+  },
+  {
+    name: "Migration",
+    path: "/migration",
+  },
+  {
+    name: "Swap",
+    url: "https://app.dsswap.io/info",
+  },
+  {
+    name: "Bridge",
+    url: "https://app.transporter.io/?tab=token&token=LAND",
+  },
+];
