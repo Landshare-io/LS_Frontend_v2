3284b6a05a7d4d2a1ec5f38db224518089c577bb
mogw <mogwinfo@gmail.com>
2024-09-04 05:03:27 +0100

    Added global context

---
+++ src/context/GlobalContext.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
+
+interface GlobalContextType {
+  theme: 'light' | 'dark';
+  setTheme: (theme: 'light' | 'dark') => void;
+  toggleTheme: () => void;
+}
+
+// Create the context with a default value of undefined
+const GlobalContext = createContext<GlobalContextType | undefined>(undefined);
+
+// Define the props for the provider
+interface GlobalProviderProps {
+  children: ReactNode;
+}
+
+// Create a provider component
+export const GlobalProvider: React.FC<GlobalProviderProps> = ({ children }) => {
+
+  const [theme, setTheme] = useState<'light' | 'dark'>('light'); // UI theme (light or dark)
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('land-v2-theme') as 'light' | 'dark' || 'light';
+    setTheme(storedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+      const newTheme = theme === 'light' ? 'dark' : 'light';
+      setTheme(newTheme);
+      localStorage.setItem('land-v2-theme', newTheme); // Store theme preference
+  };
+
+    return (
+      <GlobalContext.Provider
+        value={{
+          theme,
+          toggleTheme,
+          setTheme
+        }}
+      >
+          {children}
+      </GlobalContext.Provider>
+    );
+};
+
+// Custom hook for using the GlobalContext
+export const useGlobalContext = () => {
+    return useContext(GlobalContext);
+};
