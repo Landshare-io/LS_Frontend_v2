f7ba5007f7d367786500c68f51379172d8a53a80
mogw <mogwinfo@gmail.com>
2024-09-04 05:04:09 +0100

    Updated page app container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,9 @@ import type { AppProps } from 'next/app';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit';
 
+import { GlobalProvider } from '../context/GlobalContext';
 import { config } from '../wagmi';
 
 const client = new QueryClient();
@@ -14,8 +15,32 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-          <Component {...pageProps} />
+        <RainbowKitProvider
+          modalSize="compact"
+          theme={
+            lightTheme({
+              fontStack: "system",
+              // fonts: {
+              //   body: `'Inter Tight', sans-serif`,
+              // },
+              // colors: {
+              //   closeButtonBackground: "#61cd81",
+              //   connectButtonBackground: "#61cd81",
+              // },
+              ...lightTheme.accentColors.green,
+              accentColorForeground: "white",
+            })
+          }
+          appInfo={
+            {
+              appName: "Landshare",
+              learnMoreUrl: "https://learnaboutcryptowallets.example",
+            }
+          }
+        >
+          <GlobalProvider>
+            <Component {...pageProps} />
+          </GlobalProvider>
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
