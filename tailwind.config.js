2e10152b164faf71a692f8d6f1d5182a3451c998
mogw <mogwinfo@gmail.com>
2024-09-04 04:53:51 +0100

    Added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "primary": "var(--color-primary)",
+        "secondary": "var(--color-secondary)",
+        "third": "var(--color-third)",
+        "vault-input": "var(--color-vault-input)",
+        "input-field": "var(--color-input-field)",
+        "text-primary": "var(--color-text-primary)",
+        "text-secondary": "var(--color-text-secondary)",
+        "text-third": "var(--color-text-third)",
+        "text-fourth": "var(--color-text-fourth)",
+        "button-primary": "var(--color-button-primary)",
+        "button-secondary": "var(--color-button-secondary)",
+        "button-third": "var(--color-button-third)",
+        "button-text-primary": "var(--color-button-text-primary)",
+        "button-text-secondary": "var(--color-button-text-secondary)",
+      },
+      screens: {
+        xs: "375px",
+        sm: "450px",
+        md: "768px",
+        mlg: "1024px",
+        lg: "1280px",
+      },
+      fontFamily: {
+        interTight: ['Inter Tight', 'sans-serif'],
+      },
+      keyframes: {
+        sparkling: {
+          '0%, 100%': { filter: 'opacity(0.5)' },
+          '50%': { filter: 'opacity(1.1)' },
+        },
+      },
+      animation: {
+        sparkling: 'sparkling 3s infinite',
+      },
+    },
+  },
+  plugins: [],
+};
