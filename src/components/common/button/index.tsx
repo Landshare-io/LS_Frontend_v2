ce3879222b0b067c51f14c2827fdc5900bbbfdc0
mogw <mogwinfo@gmail.com>
2024-09-06 05:03:03 +0100

    Updated button component

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -6,7 +6,6 @@ interface ButtonProps {
   color?: string;
   className?: string;
   textClassName?: string;
-  isTiny?: boolean;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
   smallBtn?: boolean;
@@ -17,13 +16,12 @@ export default function Button ({
   outlined,
   className,
   textClassName,
-  isTiny,
   onClick,
   disabled
 }: ButtonProps) {
   return (
     <button
-      className={`flex justify-center items-center rounded-[20px] bg-[#61cd81] border-0 dark:text-[#3c3c3c] disabled:cursor-not-allowed px-[20px] py-[5px] 
+      className={`flex justify-center items-center bg-[#61cd81] border-0 font-inter dark:text-[#3c3c3c] disabled:cursor-not-allowed 
         ${className}
       `}
       disabled={disabled}
