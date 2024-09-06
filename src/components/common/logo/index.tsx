b67f4402daae581ee61f7df7938777a674e66eb1
mogw <mogwinfo@gmail.com>
2024-09-06 20:52:39 +0100

    Updated logo component

--- src/components/common/logo/index.tsx
+++ src/components/common/logo/index.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
 import Image from 'next/image';
+import { Inter_Tight } from 'next/font/google';
 import { useGlobalContext } from "../../../context/GlobalContext";
 import logo from "../../../../public/logo.svg";
 import logoDark from "../../../../public/logo-dark.svg";
 
-export default function Logo() {
+const boldInterTight = Inter_Tight({
+  weight: "700",
+  style: "normal",
+  preload: false,
+});
+
+interface LogoProps {
+  showLogoText?: boolean;
+}
+
+export default function Logo({ showLogoText }: LogoProps) {
   const { theme } = useGlobalContext();
 
   return (
     <Link
-      className={`flex itmes-center gap-[7px] cursor-pointer`}
+      className={`flex items-center gap-[7px] cursor-pointer`}
       href="/"
     >
       <Image 
@@ -17,7 +28,9 @@ export default function Logo() {
         src={theme == 'dark' ? logoDark : logo} 
         alt="logo" 
       />
-      {/* <div className="font-bold font-[20px] leading-[24px] text-[#0E1D18] dark:text-[#ffffff]">Landshare</div> */}
+      {showLogoText && (
+        <div className={`font-bold text-[20px] leading-[24px] text-[#0E1D18] dark:text-[#ffffff] ${boldInterTight.className}`}>Landshare</div>
+      )}
     </Link>
   );
 }
