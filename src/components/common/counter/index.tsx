d1017834b7253c33cd071776fbbb18820fbd919d
mogw <mogwinfo@gmail.com>
2024-09-10 04:26:03 +0100

    Added counter component

---
+++ src/components/common/counter/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import ReactLoading from 'react-loading';
+
+interface CounterProps {
+  number: string;
+  duration: number;
+  decimal: number;
+  formatType: string;
+}
+
+const Counter = ({ number, duration, decimal, formatType }: CounterProps) => {
+  const [count, setCount] = useState("0");
+  const [isloading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let start = 0;
+    const end = parseFloat(number);
+
+    if (start === end || isNaN(end) || !isFinite(end)) {
+      setIsLoading(true);
+      return;
+    }
+
+    setIsLoading(false);
+    let incrementTime = duration * 5;
+    let ctimer = setInterval(() => {
+      if (Number(start.toFixed(decimal)) > Number(end.toFixed(decimal))) {
+        clearInterval(ctimer);
+        return;
+      }
+      setCount(String(start.toFixed(decimal)))
+      start += end / duration;
+    }, incrementTime);
+
+    return () => clearInterval(ctimer);
+  }, [number, duration, decimal]);
+
+  const renderContent = () => {
+    if (isloading) {
+      return (
+        <ReactLoading type={"spin"} width={"24px"} height={"24px"} color={"#000"} />
+      );
+    }
+  
+    const formattedNumber = Number(count).toLocaleString(undefined, { maximumFractionDigits: decimal });
+  
+    if (formatType === 'percent') {
+      return `${formattedNumber}%`;
+    } else if (formatType === 'dollar') {
+      return `$${formattedNumber}`;
+    } else {
+      return formattedNumber;
+    }
+  };
+
+  return (
+    <div className="Count h-[24px] md:h-[36px]">
+      {renderContent()}
+    </div>
+  );
+}
+
+export default Counter;
