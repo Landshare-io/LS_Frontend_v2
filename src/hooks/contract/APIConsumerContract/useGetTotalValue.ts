976f6cce6cab81abd2357f4ac5db26ef33008ac0
mogw <mogwinfo@gmail.com>
2024-09-11 05:04:24 +0100

    Added APIConsumerContract useGetTotalValue hook

---
+++ src/hooks/contract/APIConsumerContract/useGetTotalValue.ts
@@ -0,0 +1,15 @@
+import { useReadContract } from "wagmi";
+import { bsc } from "viem/chains";
+import APIConsumerAbi from "../../../abis/APIConsumer.json";
+import { APICONSUMERCONTRACT_ADDRESS } from "../../../config/constants/environments";
+
+export default function useGetTotalValue() {
+  const { data } = useReadContract({
+    address: APICONSUMERCONTRACT_ADDRESS,
+    abi: APIConsumerAbi,
+    functionName: "getTotalValue",
+    chainId: bsc.id
+  })
+
+  return data
+}
