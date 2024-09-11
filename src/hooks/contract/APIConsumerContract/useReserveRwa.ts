122d1e09e43ffac9c7f1806f44aa8f83514e8617
mogw <mogwinfo@gmail.com>
2024-09-11 05:04:53 +0100

    Added APIConsumerContract useReserceRwa hook

---
+++ src/hooks/contract/APIConsumerContract/useReserveRwa.ts
@@ -0,0 +1,16 @@
+import { useReadContract } from "wagmi";
+import { bsc } from "viem/chains";
+import APIConsumerAbi from "../../../abis/APIConsumer.json";
+import { config } from "../../../wagmi";
+import { APICONSUMERCONTRACT_ADDRESS } from "../../../config/constants/environments";
+
+export default function useGetTotalValue() {
+  const { data } = useReadContract({
+    address: APICONSUMERCONTRACT_ADDRESS,
+    abi: APIConsumerAbi,
+    functionName: "reserveRWA",
+    chainId: bsc.id
+  })
+
+  return data
+}
