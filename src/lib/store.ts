d1bba9e6de6c08531959d4fe072c7d7d7ec72168
mogw <mogwinfo@gmail.com>
2024-09-13 03:27:38 +0100

    Updated redux store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,10 +1,14 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit";
 import { combineSlices, configureStore } from "@reduxjs/toolkit";
 import { apiConsumerContractSlice } from "./slices/contract-slices/APIConsumerContractSlice";
+import { firebasePropertiesRentalSlice } from "./slices/firebase-slices/properties-rental";
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices(apiConsumerContractSlice);
+const rootReducer = combineSlices(
+  apiConsumerContractSlice,
+  firebasePropertiesRentalSlice
+);
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>;
 
