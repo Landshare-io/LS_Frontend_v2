3ec503863362d52d2712b5da4da322a994433cef
mogw <mogwinfo@gmail.com>
2024-09-13 20:35:38 +0100

    Updated PancakePairContract hook

--- src/hooks/contract/PancakePairContract/useGetReserves.ts
+++ src/hooks/contract/PancakePairContract/useGetReserves.ts
@@ -4,12 +4,18 @@ import PancakePairAbi from '../../../abis/PancakePair.json';
 import { PANCAKEPAIRCONTRACT_ADDRESS } from "../../../config/constants/environments";
 
 export default function useGetReserves() {
-  const { data } = useReadContract({
+  const { data, isError, isLoading, error } = useReadContract({
     address: PANCAKEPAIRCONTRACT_ADDRESS,
     abi: PancakePairAbi,
     functionName: "getReserves",
     chainId: bsc.id
   })
+
+  if (isLoading) return [0, 0]
+  if (isError) {
+    console.log('Fetching PancakePairContract userInfo error', error)
+    return [0, 0]
+  }
 
   return data
 }
