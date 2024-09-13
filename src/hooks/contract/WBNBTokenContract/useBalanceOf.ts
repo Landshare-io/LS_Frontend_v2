fcf0d27c76cf06ebb6c1c92bad185e366f2dad40
mogw <mogwinfo@gmail.com>
2024-09-13 20:35:49 +0100

    Updated WBNBTokenContract hook

--- src/hooks/contract/WBNBTokenContract/useBalanceOf.ts
+++ src/hooks/contract/WBNBTokenContract/useBalanceOf.ts
@@ -8,13 +8,19 @@ interface UseBalanceOfProps {
 }
 
 export default function useBalanceOf({ address }: UseBalanceOfProps) {
-  const { data } = useReadContract({
+  const { data, isError, isLoading, error } = useReadContract({
     address: WBNBTOKENCONTRACT_ADDRESS,
     abi: WBNBAbi,
     functionName: "balanceOf",
     chainId: bsc.id,
     args: [address]
   })
+
+  if (isLoading) return 0
+  if (isError) {
+    console.log('Fetching WBNBTokenContract balanceOf error', error)
+    return 0
+  }
 
   return data
 }
