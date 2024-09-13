0fa4a9c33b7827bd344ec8e86a36f080ebb5cbe8
mogw <mogwinfo@gmail.com>
2024-09-13 20:34:50 +0100

    Updated BNBApePairContract hook

--- src/hooks/contract/BNBApePairContract/useGetReserves.ts
+++ src/hooks/contract/BNBApePairContract/useGetReserves.ts
@@ -4,12 +4,18 @@ import BNBApePair from "../../../abis/BNBApePair.json";
 import { BNBAPEPAIRCONTRACT_ADDRESS } from "../../../config/constants/environments";
 
 export default function useGetReserves() {
-  const { data } = useReadContract({
+  const { data, isError, isLoading, error } = useReadContract({
     address: BNBAPEPAIRCONTRACT_ADDRESS,
     abi: BNBApePair,
     functionName: "getReserves",
     chainId: bsc.id
   })
+
+  if (isLoading) return [0, 0]
+  if (isError) {
+    console.log('Fetching BNBApePairContract getReserves error', error)
+    return [0, 0]
+  }
 
   return data
 }
