fb9a00fadc2e03b0eeb621ce0e8ba0db8ac0854a
mogw <mogwinfo@gmail.com>
2024-09-16 20:03:59 +0100

    Added home rwa section component

---
+++ src/components/home-rwa-section/index.tsx
@@ -0,0 +1,13 @@
+import StatusCard from "../status-card"
+import HomeRwaHeroSection from "./hero-section"
+
+export default function HomeRwaSection() {
+  
+
+  return (
+    <div className="bg-primary">
+      <HomeRwaHeroSection />
+      <StatusCard />
+    </div>
+  )
+}
