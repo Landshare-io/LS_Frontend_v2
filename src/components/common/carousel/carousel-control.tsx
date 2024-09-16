b85da178feab18f9d9745ded3dc8c0f15eee315e
mogw <mogwinfo@gmail.com>
2024-09-16 20:02:45 +0100

    Updated carousel control component

--- src/components/common/carousel/carousel-control.tsx
+++ src/components/common/carousel/carousel-control.tsx
@@ -2,21 +2,19 @@ import React, { useEffect, useState } from "react";
 
 interface CarouselControlProps {
   count: number; 
-  isIndicator: boolean;
+  paused: boolean;
   activeIndex: number;
   setActiveIndex: Function;
   carouselControlClass: string;
 }
 
 export default function CarouselControl({ 
-  count, 
-  isIndicator,
+  count,
+  paused,
   activeIndex,
   setActiveIndex,
   carouselControlClass,
 }: CarouselControlProps) {
-  const [paused, setPaused] = useState(false);
-
   const updateIndex = (newIndex: number) => {
     if (newIndex < 0) {
       newIndex = count - 1;
@@ -42,14 +40,12 @@ export default function CarouselControl({
   });
 
   return (
-    <div
-      className={`flex justify-between items-center top-[300px] md:top-[250px] px-[20px] ${carouselControlClass}`}
-    >
-      <div className={`flex visibility-hidden ${isIndicator ? 'visibility-visible' : ''}`}>
+    <div className={`flex justify-center md:justify-between items-center top-[300px] md:top-[250px] px-[20px] ${carouselControlClass}`}>
+      <div className="flex gap-[10px] visible">
         {Array.apply(null, Array(count)).map((child, index) => {
           return (
             <div
-              className={`w-[12px] h-[12px] opacity-[0.2] bg-[#00352e] rounded-[16px] border-0 outline-0 padding-0 dark:bg-[#cacaca] ${index === activeIndex ? 'rounded-[16px] opacity-1 bg-[#61cd81]' : ''}`}
+              className={`w-[12px] h-[12px] opacity-20 bg-[#00352e] rounded-[16px] border-0 outline-0 padding-0 dark:bg-[#cacaca] cursor-pointer ${index === activeIndex ? '!opacity-100 !bg-[#61cd81]' : ''}`}
               key={`indicator-${index}`}
               onClick={() => {
                 updateIndex(index);
