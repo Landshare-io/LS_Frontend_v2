d429b34f7b7353a779b8d1bd20c77bf21ed9849c
mogw <mogwinfo@gmail.com>
2024-09-19 03:42:49 +0100

    Updated property card component

--- src/components/common/property-card/index.tsx
+++ src/components/common/property-card/index.tsx
@@ -50,34 +50,34 @@ export default function PropertyCard({ property, style, active }) {
               {property.type}
             </span>
           </div>
-          <div className={`property-card-description-section ${active ? 'bg-tw-third' : ''}`}>
+          <div className={`property-card-description-section ${active ? 'bg-third' : ''}`}>
             <div className="property-card-price-wrapper">
               <div className="property-card-price-detail">
-                <span className="property-card-label text-tw-text-third">Property Value</span>
-                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className="property-card-value text-tw-text-primary">${propertyValue.toLocaleString()}</span>)}
+                <span className="property-card-label text-text-third">Property Value</span>
+                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className="property-card-value text-text-primary">${propertyValue.toLocaleString()}</span>)}
               </div>
               <div className="property-card-price-detail">
-                <span className="property-card-label text-tw-text-third">Rental Yield</span>
-                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className="property-card-value text-tw-text-primary">{((property.grossRent * (1 - property.management) * 12 - property.insurance - property.tax) / propertyValue * 100).toFixed(2)}%</span>)}
+                <span className="property-card-label text-text-third">Rental Yield</span>
+                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className="property-card-value text-text-primary">{((property.grossRent * (1 - property.management) * 12 - property.insurance - property.tax) / propertyValue * 100).toFixed(2)}%</span>)}
               </div>
               <div className="property-card-price-detail">
-                <span className="property-card-label text-tw-text-third">Ann. Return</span>
-                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className="property-card-value text-tw-text-primary">{((property.grossRent * (1 - property.management) * 12 - property.insurance - property.tax) / propertyValue * 100 + property.appreciation).toFixed(2)}%</span>)}
+                <span className="property-card-label text-text-third">Ann. Return</span>
+                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className="property-card-value text-text-primary">{((property.grossRent * (1 - property.management) * 12 - property.insurance - property.tax) / propertyValue * 100 + property.appreciation).toFixed(2)}%</span>)}
               </div>
             </div>
             <div className="property-card-details-wrapper">
-              <span className="property-card-type bg-tw-primary">Single Family</span>
+              <span className="property-card-type bg-primary">Single Family</span>
               <div className="property-card-inventories">
                 {property?.bedrooms &&
                   <div className="property-card-furniture">
                     <img src={IconSofa} alt="sofa" />
-                    <div className="text-tw-text-primary">{property?.bedrooms}</div>
+                    <div className="text-text-primary">{property?.bedrooms}</div>
                   </div>
                 }
                 {property?.bathrooms &&
                   <div className="property-card-furniture">
                     <img src={IconBathroom} alt="sofa" />
-                    <div className="text-tw-text-primary">{property?.bathrooms}</div>
+                    <div className="text-text-primary">{property?.bathrooms}</div>
                   </div>
                 }
               </div>
