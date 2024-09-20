f623fd76351679f3f0c41dd088517849272a341d
mogw <mogwinfo@gmail.com>
2024-09-20 22:10:44 +0100

    Added useFetchRwa graphql hook

---
+++ src/hooks/apollo/useFetchRwa.ts
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { formatEther } from "ethers";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { LAND_PRICE_SUBGRAPH_URL } from "../../config/constants/environments";
+
+export default function useFetchRwa() {
+  const [rwaData, setRwaData] = useState<any[]>([])
+  
+  useEffect(() => {
+    (async () => {
+      const pricesQuery = `
+        query{
+          valueUpdateds {
+            blockTimestamp
+            value
+            id
+          }
+        }
+      `;
+      const client = new ApolloClient({
+        uri: LAND_PRICE_SUBGRAPH_URL,
+        cache: new InMemoryCache(),
+      });
+      const rwaData = await client
+        .query({
+          query: gql(pricesQuery),
+        })
+      if (rwaData) {
+        const returnData: any[] = [];
+        rwaData.data.valueUpdateds.filter((value: any) => Number(value.blockTimestamp) > 1702900000).sort((a: any, b: any) => {
+          return Number(a.blockTimestamp) - Number(b.blockTimestamp);
+        }).map((data: any) => {
+          returnData.push([
+            Number(data.blockTimestamp) * 1000,
+            parseFloat(Number(formatEther(data.value)).toFixed(6))
+          ])
+        });
+        returnData.push([
+          Date.now(),
+          returnData[returnData.length - 1][1]
+        ]);
+        setRwaData(returnData);
+      }
+    })()
+  }, [])
+
+  return rwaData
+}
