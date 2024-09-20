46dabeb15c6f9435906ae26de1bd1568467d96a4
mogw <mogwinfo@gmail.com>
2024-09-20 05:08:13 +0100

    Added useGetLandPrice hook

---
+++ src/hooks/axios/useGetLandPrice.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { LANDMARKET_URL } from "../../config/constants/environments";
+
+export default function useBankBalance() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [price, setPrice] = useState(0);
+  const [circulatingSupply, setCirculatingSupply] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true)
+      const { data: { priceData } } = await axios.get(LANDMARKET_URL);
+      const { data: circulatingSupplyData } = await axios.get('https://api.landshare.io/api/test?q=circulating');
+
+      setPrice(priceData)
+      setCirculatingSupply(circulatingSupplyData)
+      setIsLoading(false)
+    })()
+  }, [])
+
+  return {
+    price,
+    isLoading,
+    circulatingSupply
+  }
+}
