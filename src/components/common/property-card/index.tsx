d4c9c38dfb6b5a1cd4d5decfe9af906d19993ba9
mogw <mogwinfo@gmail.com>
2024-09-20 05:10:25 +0100

    Updated common property card component

--- src/components/common/property-card/index.tsx
+++ src/components/common/property-card/index.tsx
@@ -1,95 +1,75 @@
-import React, { useMemo, useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import IconSofa from "../../assets/img/new/sofa.svg";
-import IconBathroom from "../../assets/img/new/bathroom.svg";
-import IconLocation from "../../assets/img/new/location.svg";
-import "./property-card.css";
+import Image from "next/image";
+import IconSofa from "../../../../public/icons/sofa.svg";
+import IconBathroom from "../../../../public/icons/bathroom.svg";
+import IconLocation from "../../../../public/icons/location.svg";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import { getPropertyValues } from "../LandshareAPIConsumer";
-import { useGlobalContext } from "../../contexts/GlobalContext";
+import useGetPropertyValues from "../../../hooks/contract/APIConsumerContract/useGetPropertyValues";
+import { useGlobalContext } from "../../../context/GlobalContext";
+import { BigNumberish, formatEther } from "ethers";
+import { BOLD_INTER_TIGHT } from "../../../config/constants/environments";
 
-export default function PropertyCard({ property, style, active }) {
-  const { isDarkMode } = useGlobalContext();
+interface PropertyCardProps {
+  property: any;
+}
+
+export default function PropertyCard({ property }: PropertyCardProps) {
+  const { theme } = useGlobalContext();
   const router = useRouter();
-  const [propertyValue, setPropertyValue] = useState(0)
-  const [isLoading, setIsLoading] = useState(false);
-  useMemo(async () => {
-    setIsLoading(true);
-    if (property.coreLogicID) {
-      const tt = property.coreLogicID
-      try {
-        const value = await getPropertyValues(tt);
-        setPropertyValue(value)
-      } catch (e) {
-        console.log(e)
-      }
-    }
-    if (propertyValue !== 0 && isFinite(parseFloat(propertyValue)) && propertyValue !== null && typeof propertyValue !== 'undefined')
-      setIsLoading(false);
-  }, [property])
+  const {
+    isLoading,
+    data: propertyValue
+  } = useGetPropertyValues(property?.coreLogicID ?? '0') as {
+    isLoading: boolean,
+    data: BigNumberish
+  }
 
-  useEffect(() => {
-    if (propertyValue !== 0 && isFinite(parseFloat(propertyValue)) && propertyValue !== null && typeof propertyValue !== 'undefined')
-      setIsLoading(false);
-  }, [propertyValue])
   return (
-    <div className="property-card-wrapper cursor-pointer">
-      <div className={`property-card-container ${active ? 'active' : ''}`} style={style} onClick={() => router.push(`/tokenized-asset/${property?.id}`)}>
-        <SkeletonTheme baseColor={`${isDarkMode ? "#31333b" : "#dbdde0"}`} highlightColor={`${isDarkMode ? "#52545e" : "#f6f7f9"}`}>
-          <div className="property-card-container property-card-preview-div">
-            <img className="property-card-preview" src={property?.pictures ? property?.pictures[0] : "sfsdf"} alt="property image" />
-            <div className="property-card-title">
-              <img src={IconLocation} alt="location" />
-              <div>{property.address}</div>
+    <div className="pb-[10px] md:w-fit md:rounded-[24px] md:px-[20px] md:pb-[24px] max-w-[501px] relative cursor-pointer">
+      <div className="relative rounded-[24px] shadow shadow-lg z-[3]" onClick={() => router.push(`/tokenized-asset/${property?.id}`)}>
+        <SkeletonTheme baseColor={`${theme == 'dark' ? "#31333b" : "#dbdde0"}`} highlightColor={`${theme == 'dark' ? "#52545e" : "#f6f7f9"}`}>
+          <div className="relative rounded-[24px] shadow shadow-lg z-[3] w-full md:w-[390px] md:h-[292px]">
+            <img className="w-full h-full object-cover rounded-t-[24px]" src={property?.pictures[0]} alt="property image" />
+            <div className="absolute text-[14px] leading-[22px] tracking-[0.02em] text-[#ffffff] flex gap-[7px] left-0 bottom-0 bg-gradient-to-b from-transparent to-[#00000096] py-[4px] px-[8px] w-full">
+              <Image src={IconLocation} alt="location" />
+              <div className="overflow-hidden text-ellipsis whitespace-nowrap">{property.address}</div>
             </div>
-            <span className="property-card-badge"
-              style={{
-                background: property.type == "Rental Property" ? "#FF5454" : "#3B98EE"
-              }}
-            >
+            <span className={`absolute left-0 top-[20px] shadow-md rounded-r-[15px] text-[12px] leading-[20px] tracking-[0.02em] text-[0.02em] text-[#fff] py-[4px] px-[15px] ${property.type == "Rental Property" ? "bg-[#FF5454]" : "bg-[#3B98EE]"} ${BOLD_INTER_TIGHT.className}`}>
               {property.type}
             </span>
           </div>
-          <div className={`property-card-description-section ${active ? 'bg-third' : ''}`}>
-            <div className="property-card-price-wrapper">
-              <div className="property-card-price-detail">
-                <span className="property-card-label text-text-third">Property Value</span>
-                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className="property-card-value text-text-primary">${propertyValue.toLocaleString()}</span>)}
+          <div className="flex flex-col py-[20px] px-[30px] gap-[24px] bg-third rounded-b-[24px]">
+            <div className="flex justify-between">
+              <div className="flex flex-col items-center">
+                <span className="text-[12px] leading-[22px] text-center tracking-[0.24px] text-text-third">Property Value</span>
+                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className={`text-[24px] leading-[30px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>${Number(formatEther(propertyValue)).toLocaleString()}</span>)}
               </div>
-              <div className="property-card-price-detail">
-                <span className="property-card-label text-text-third">Rental Yield</span>
-                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className="property-card-value text-text-primary">{((property.grossRent * (1 - property.management) * 12 - property.insurance - property.tax) / propertyValue * 100).toFixed(2)}%</span>)}
+              <div className="flex flex-col items-center">
+                <span className="text-[12px] leading-[22px] text-center tracking-[0.24px] text-text-third">Rental Yield</span>
+                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className={`text-[24px] leading-[30px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>{((property.grossRent * (1 - property.management) * 12 - property.insurance - property.tax) / Number(formatEther(propertyValue)) * 100).toFixed(2)}%</span>)}
               </div>
-              <div className="property-card-price-detail">
-                <span className="property-card-label text-text-third">Ann. Return</span>
-                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className="property-card-value text-text-primary">{((property.grossRent * (1 - property.management) * 12 - property.insurance - property.tax) / propertyValue * 100 + property.appreciation).toFixed(2)}%</span>)}
+              <div className="flex flex-col items-center">
+                <span className="text-[12px] leading-[22px] text-center tracking-[0.24px] text-text-third">Ann. Return</span>
+                {isLoading ? (<Skeleton className="rounded-lg" width={80} height={26} />) : (<span className={`text-[24px] leading-[30px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>{((property.grossRent * (1 - property.management) * 12 - property.insurance - property.tax) / Number(formatEther(propertyValue)) * 100 + property.appreciation).toFixed(2)}%</span>)}
               </div>
             </div>
-            <div className="property-card-details-wrapper">
-              <span className="property-card-type bg-primary">Single Family</span>
-              <div className="property-card-inventories">
+            <div className="py-0 px-[10px] sm:p-0 mb-[15px] flex justify-between">
+              <span className="text-[12px] leading-[20px] md:text-[14px] md:leading-[22px] rounded-[20px] tracking-[0.02em] md:text-[14px] md:leading-[22px] text-[#61CD81] md:py-[4px] md:px-[16px] bg-primary">Single Family</span>
+              <div className="flex gap-[10px] md:gap-[20px]">
                 {property?.bedrooms &&
-                  <div className="property-card-furniture">
-                    <img src={IconSofa} alt="sofa" />
-                    <div className="text-text-primary">{property?.bedrooms}</div>
+                  <div className="flex items-center gap-[5px]">
+                    <Image src={IconSofa} alt="sofa" />
+                    <div className="text-[12px] leading-[20px] text-[14px] font-medium leading-[22px] text-text-primary">{property?.bedrooms}</div>
                   </div>
                 }
                 {property?.bathrooms &&
-                  <div className="property-card-furniture">
-                    <img src={IconBathroom} alt="sofa" />
-                    <div className="text-text-primary">{property?.bathrooms}</div>
+                  <div className="flex items-center gap-[5px]">
+                    <Image src={IconBathroom} alt="bathroom" />
+                    <div className="text-[12px] leading-[20px] text-[14px] font-medium leading-[22px] text-text-primary">{property?.bathrooms}</div>
                   </div>
                 }
               </div>
             </div>
-            {/* <div className="property-card-button-group">
-              <Button outlined style={{ width: "100%" }} smallBtn>
-                View Details
-              </Button>
-              <Button style={{ width: "100%" }} color="#fff" smallBtn>
-                Invest Now
-              </Button>
-            </div> */}
           </div>
         </SkeletonTheme>
       </div>
