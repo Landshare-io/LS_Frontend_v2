e4190ba0b868bef4f9b3b65e768900f7f17f1777
mogw <mogwinfo@gmail.com>
2024-09-20 05:10:07 +0100

    Added financial property card component

--- src/components/financial-property-card/index.tsx
+++ src/components/financial-property-card/index.tsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import { BsInfoCircle } from "react-icons/bs";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import OverlayTrigger from "../common/overlay-trigger";
-import IconRefresh from "../../../assets/img/new/refresh.svg";
-import capRate from "../../../assets/img/icons/Dollars.svg";
-import Est from "../../../assets/img/icons/Raised.svg";
-import myRwa from "../../../assets/img/icons/Coin_Stacked.svg";
+import IconRefresh from "../../../public/icons/refresh.svg";
+import capRate from "../../../public/icons/dollars.svg";
+import Est from "../../../public/icons/raised.svg";
+import myRwa from "../../../public/icons/coin-stacked-small.svg";
 import { useGlobalContext } from "../../context/GlobalContext";
 import { StringKeyStringValueObject } from "../../utils/type";
+import { BOLD_INTER_TIGHT } from "../../config/constants/environments";
 
 const iconUrl: StringKeyStringValueObject = {
   "Rental Yield": capRate,
@@ -45,7 +47,7 @@ export default function FinancialPropertyCard ({ title, value, loading, page }: 
   return (
     <div className={`${page == "rwa" ? "bg-third" : "bg-third md:bg-secondary"} rwa-property-card flex flex-col justify-center items-center md:items-start`}>
       <div className="flex flex-col gap-[10px] rwa-property-card-detail">
-        <span className="text-text-secondary rwa-property-card-title flex gap-1 items-center relative">
+        <span className="text-text-secondary justify-center font-medium text-[14px] leading-[22px] min-w-[180px] md:justify-start flex gap-1 items-center relative">
           {title}
           {tips[title] != "" && <OverlayTrigger
             placement="bottom"
@@ -60,11 +62,15 @@ export default function FinancialPropertyCard ({ title, value, loading, page }: 
           baseColor={`${theme == 'dark' ? "#31333b" : "#dbdde0"}`} 
           highlightColor={`${theme == 'dark' ? "#52545e" : "#f6f7f9"}`}
         >
-          <div className="rwa-property-card-description">
-            <div className="bg-primary rwa-property-card-description-image">
-              <img src={iconUrl[title] ? iconUrl[title] : iconUrl["Est. Appreciation"]} alt="refresh" width="18px" />
+          <div className="flex gap-[8px]">
+            <div className="bg-primary w-[32px] h-[32px] py-[6.23px] px-[7px] flex items-center justify-center rounded-full">
+              <Image src={iconUrl[title] ? iconUrl[title] : iconUrl["Est. Appreciation"]} alt="refresh" className="w-[18px]" />
             </div>
-            {currentLoading == true || value == "NaN%" || value == "Infinity%" ? (<Skeleton className="rounded-lg" width={100} height={28} />) : (<span className="text-text-primary">{value}</span>)}
+            {currentLoading == true || value == "NaN%" || value == "Infinity%" ? (
+              <Skeleton className="rounded-lg" width={100} height={28} />
+            ) : (
+              <span className={`text-text-primary text-[24px] leading-[30px] ${BOLD_INTER_TIGHT.className}`}>{value}</span>
+            )}
           </div>
         </SkeletonTheme>
       </div>
