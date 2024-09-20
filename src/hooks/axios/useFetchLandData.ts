21f174c2410424f29ed7fdf91e2c1621f02c34b8
mogw <mogwinfo@gmail.com>
2024-09-20 22:11:07 +0100

    Added useFetchLandData axios hook

---
+++ src/hooks/axios/useFetchLandData.ts
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { LANDPRICE_URL } from "../../config/constants/environments";
+
+export type PRICE_DATA = {
+  prices: any[],
+  market_caps: any[],
+  total_volumes: any[]
+}
+
+export default function useFetchLandData(dueTimeStamp: Date, now: number) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [price, setPrice] = useState<PRICE_DATA>({ 
+    prices: [], 
+    market_caps: [], 
+    total_volumes: []
+  });
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true)
+      const { data: { data: { data: { quotes: priceData } } } } = await axios.get(LANDPRICE_URL);
+      let market_caps = [];
+      let prices = [];
+      let total_volumes = [];
+      for (let i = 0; i < priceData?.length; i++) {
+        if (Math.floor(Date.parse(priceData[i].timestamp) / 1000) >= Math.floor(Number(dueTimeStamp) / 1000) && Math.floor(Date.parse(priceData[i].timestamp) / 1000) <= Math.floor(Number(now) / 1000)) {
+          let market_cap = [
+            priceData[i].timestamp, priceData[i].quote.USD.market_cap
+          ];
+          let total_volume = [
+            priceData[i].timestamp, priceData[i].quote.USD.volume_24h
+          ];
+          let price = [
+            priceData[i].timestamp, priceData[i].quote.USD.price
+          ];
+          market_caps.push(market_cap);
+          prices.push(price);
+          total_volumes.push(total_volume);
+        }
+      }
+      let returnData = { prices: prices, market_caps: market_caps, total_volumes: total_volumes };
+      setPrice(returnData);
+      setIsLoading(false)
+    })()
+  }, [])
+
+  return {
+    price,
+    isLoading
+  }
+}
