a62b8606499007cd4907ac6f8a532c5eb4148684
mogw <mogwinfo@gmail.com>
2024-09-24 15:46:24 +0100

    Added useGetAllTokens API hook

---
+++ src/hooks/axios/useGetAllTokens.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { LIVE_COIN_LIST_URL, LIVE_COIN_API_KEY } from "../../config/constants/environments";
+
+export default function useGetAllTokens() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [allTokens, setAllTokens] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true)
+      const res = await fetch(
+        new Request(LIVE_COIN_LIST_URL),
+        {
+          method: "POST",
+          headers: new Headers({
+            "content-type": "application/json",
+            "x-api-key": LIVE_COIN_API_KEY,
+          }),
+          body: JSON.stringify({
+            currency: "USD",
+            sort: "rank",
+            order: "ascending",
+            offset: 0,
+            limit: 50,
+            meta: true,
+          }),
+        }
+      );
+      setAllTokens(await res.json());
+      setIsLoading(false)
+    })()
+  }, [])
+
+  return {
+    allTokens,
+    isLoading
+  }
+}
