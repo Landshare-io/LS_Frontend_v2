f80a7f1a13a9a082bf1017c60b69ff3e014c1dc3
mogw <mogwinfo@gmail.com>
2024-09-25 16:04:37 +0100

    Added LandToken contract's useApprove hook

---
+++ src/hooks/contract/LandTokenContract/useApprove.ts
@@ -0,0 +1,29 @@
+import { useWriteContract } from 'wagmi'
+import LpTokenV2Abi from "../../../abis/LpTokenV2.json";
+import { LAND_TOKEN_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+import { Address } from 'viem';
+import { BigNumberish } from 'ethers';
+
+export default function useApprove() {
+  const {
+    data,
+    isPending,
+    writeContract
+  } = useWriteContract();
+
+  async function approve(chainId: number, approveAddress: Address, amount: number | BigNumberish) {
+    await writeContract({
+      address: LAND_TOKEN_CONTRACT_ADDRESS[chainId ?? 56],
+      abi: LpTokenV2Abi,
+      functionName: "approve",
+      chainId: chainId ?? 56,
+      args: [approveAddress, amount]
+    });
+  }
+
+  return {
+    approve,
+    isPending,
+    data
+  }
+}
