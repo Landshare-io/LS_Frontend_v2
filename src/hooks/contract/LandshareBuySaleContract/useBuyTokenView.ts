e00ffd95ee6a3356e50812d2ef1e1cac5aed98b8
mogw <mogwinfo@gmail.com>
2024-09-25 16:02:30 +0100

    Added LandshareBuySale contract's useBuyToken hook

---
+++ src/hooks/contract/LandshareBuySaleContract/useBuyTokenView.ts
@@ -0,0 +1,30 @@
+import { useReadContract } from "wagmi";
+import { bsc } from "viem/chains";
+import LandshareBuySaleAbi from "../../../abis/LandshareBuySale.json"
+import { LANDSHARE_BUY_SALE_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+import { BigNumberish } from "ethers";
+import { Address } from "viem";
+
+export default function useBuyTokenView(amountOfSecurities: number | BigNumberish, stableCoinAddress: Address) {
+  const { data, isError, isLoading, error } = useReadContract({
+    address: LANDSHARE_BUY_SALE_CONTRACT_ADDRESS,
+    abi: LandshareBuySaleAbi,
+    functionName: "buyTokenView",
+    chainId: bsc.id,
+    args: [amountOfSecurities, stableCoinAddress]
+  })
+
+  if (isLoading) return {
+    amountOfStableCoin: 0,
+    amountOfLAND: 0
+  }
+  if (isError) {
+    console.log('Fetching LandshareBuySaleContract buyTokenView error', error)
+    return {
+      amountOfStableCoin: 0,
+      amountOfLAND: 0
+    }
+  }
+
+  return data
+}
