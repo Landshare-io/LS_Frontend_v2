0b32b4acbae2252b0d5c0e023cc08bc372caf610
mogw <mogwinfo@gmail.com>
2024-09-27 13:51:03 +0100

    Updated useIsOptedIn hook

--- src/hooks/contract/AutoRedeemContract/useIsOptedIn.ts
+++ src/hooks/contract/AutoRedeemContract/useIsOptedIn.ts
@@ -1,16 +1,16 @@
 import { useReadContract } from "wagmi";
+import { bsc } from "viem/chains";
 import type { Address } from "viem";
 import AutoRedeemAbi from "../../../abis/AutoRedeem.json";
 import { AUTO_REDEEM_CONTRACT_ADDRESS } from "../../../config/constants/environments";
 
 export default function useIsOptedIn(address: Address | undefined) {
-  if (typeof address == "undefined") return false
-
   const { data, isError, isLoading, error } = useReadContract({
     address: AUTO_REDEEM_CONTRACT_ADDRESS,
     abi: AutoRedeemAbi,
     functionName: "isOptedIn",
-    args: [address]
+    args: [address],
+    chainId: bsc.id
   })
 
   if (isLoading) return false
