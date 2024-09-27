953a456b553477702708a98588329d7ed03450df
mogw <mogwinfo@gmail.com>
2024-09-27 13:50:31 +0100

    Fixed issue of useGetAllowedToTransfer hook

--- src/hooks/contract/RWAContract/useGetAllowedToTransfer.ts
+++ src/hooks/contract/RWAContract/useGetAllowedToTransfer.ts
@@ -1,13 +1,13 @@
 import { useReadContract } from "wagmi";
 import { bsc } from "viem/chains";
-import LandshareSaleAbi from "../../../abis/LandshareSale.json";
-import { LANDSHARE_SALE_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+import RwaContractAbi from "../../../abis/RWAContract.json";
+import { RWA_CONTRACT_ADDRESS } from "../../../config/constants/environments";
 import { Address } from "viem";
 
 export default function useGetAllowedToTransfer(address: Address | undefined) {
   const { data, isError, isLoading, error } = useReadContract({
-    address: LANDSHARE_SALE_CONTRACT_ADDRESS,
-    abi: LandshareSaleAbi,
+    address: RWA_CONTRACT_ADDRESS,
+    abi: RwaContractAbi,
     functionName: "getAllowedToTransfer",
     chainId: bsc.id,
     args: [address]
