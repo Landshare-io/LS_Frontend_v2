9197db8a52ed44ba310c949ae267c6cee9e017be
mogw <mogwinfo@gmail.com>
2024-09-30 15:18:38 +0100

    Added withdraw functionality to vaultBalanceManual hook

--- src/hooks/contract/vault/useVaultBalanceManual.tsx
+++ src/hooks/contract/vault/useVaultBalanceManual.tsx
@@ -1,30 +1,72 @@
 import { useState, useEffect } from "react"
 import { useWaitForTransactionReceipt } from "wagmi";
-import { formatEther } from "ethers";
+import { formatEther, BigNumberish } from "ethers";
 import { bsc } from "viem/chains";
-import useDeposit from "../AutoVaultV2Contract/useDeposit";
-import useBalanceOf from "../LpTokenV2Contract/useBalanceOf";
-import { BigNumberish } from "ethers";
 import { Address } from "viem";
+import useDeposit from "../AutoVaultV2Contract/useDeposit";
+import useWithdraw from "../AutoVaultV2Contract/useWithdraw";
+import useBalanceOfLpTokenV2 from "../LpTokenV2Contract/useBalanceOf";
+import useAllowanceOfLpTokenV2 from "../LpTokenV2Contract/useAllowance";
+import useTotalStaked from "../MasterchefContract/useTotalStaked";
+import useUserInfo from "../MasterchefContract/useUserInfo";
+import usePendingLand from "../MasterchefContract/usePendingLand";
+import { AUTO_VAULT_V3_CONTRACT_ADDRESS, MASTERCHEF_CONTRACT_ADDRESS } from "../../../config/constants/environments";
 
-export default function useVaultBalanceManual(address: Address) {
+export default function useVaultBalanceManual(address: Address | undefined) {
   const [transactionState, setTransactionState] = useState("")
   const { deposit, data: depositTx } = useDeposit()
-  const lpTokenV2Balance = useBalanceOf({ address }) as BigNumberish
+  const { withdraw, data: withdrawTx } = useWithdraw()
+  const { data: lpTokenV2Balance, refetch: refetchLpTokenV2 } = useBalanceOfLpTokenV2({ address }) as {
+    data: BigNumberish,
+    refetch: Function
+  }
+  const { refetch: refetchAllowanceOfLpTokenV2OfVault } = useAllowanceOfLpTokenV2(address, AUTO_VAULT_V3_CONTRACT_ADDRESS)
+  const { refetch: refetchAllowanceOfLpTokenV2OfMasterChef } = useAllowanceOfLpTokenV2(address, MASTERCHEF_CONTRACT_ADDRESS)
+  const { refetch: refetchTotalStaked } = useTotalStaked()
+  const { refetch: refetchUserInfo } = useUserInfo({ address })
+  const { refetch: refetchPendingLand } = usePendingLand({ address })
 
-  const { isSuccess: depositSuccess, isLoading: rwaApproveLoading } = useWaitForTransactionReceipt({
+  const { isSuccess: depositSuccess } = useWaitForTransactionReceipt({
     hash: depositTx,
+    chainId: bsc.id
+  });
+  const { isSuccess: withdrawSuccess } = useWaitForTransactionReceipt({
+    hash: withdrawTx,
     chainId: bsc.id
   });
 
   useEffect(() => {
     try {
-      setTransactionState("Deposit Transaction success")
+      if (depositSuccess) {
+        refetchLpTokenV2()
+        refetchAllowanceOfLpTokenV2OfVault()
+        refetchAllowanceOfLpTokenV2OfMasterChef()
+        setTransactionState("Deposit Transaction success")
+      } else {
+        setTransactionState("Transaction failed")
+      }
     } catch (error) {
       setTransactionState("Transaction failed")
       console.log(error)
     }
   }, [depositSuccess])
+
+  useEffect(() => {
+    try {
+      if (withdrawSuccess) {
+        refetchLpTokenV2()
+        refetchTotalStaked()
+        refetchUserInfo()
+        refetchPendingLand()
+        setTransactionState("Withdraw Transaction success")
+      } else {
+        setTransactionState("Transaction failed")
+      }
+    } catch (error) {
+      setTransactionState("Transaction failed")
+      console.log(error)
+    }
+  }, [withdrawSuccess])
 
   const depositVault = (amount: BigNumberish) => {
     if (Number(formatEther(amount)) > Number(formatEther(lpTokenV2Balance))) {
@@ -35,7 +77,13 @@ export default function useVaultBalanceManual(address: Address) {
     deposit(amount)
   }
 
+  const withdrawVault = (amount: BigNumberish) => {
+    withdraw(amount)
+  }
+
   return {
-    depositVault
+    depositVault,
+    withdrawVault,
+    transactionState
   }
 }
