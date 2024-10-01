042480e34cbd5040c145aed6ed0bda34d56fcf4d
mogw <mogwinfo@gmail.com>
2024-10-01 04:35:24 +0100

    Added approveVault functionality

--- src/hooks/contract/vault/useVaultBalanceManual.tsx
+++ src/hooks/contract/vault/useVaultBalanceManual.tsx
@@ -3,19 +3,22 @@ import { useWaitForTransactionReceipt } from "wagmi";
 import { formatEther, BigNumberish } from "ethers";
 import { bsc } from "viem/chains";
 import { Address } from "viem";
+import { useGlobalContext } from "../../../context/GlobalContext";
 import useDeposit from "../AutoVaultV2Contract/useDeposit";
 import useWithdraw from "../AutoVaultV2Contract/useWithdraw";
 import useBalanceOfLpTokenV2 from "../LpTokenV2Contract/useBalanceOf";
 import useAllowanceOfLpTokenV2 from "../LpTokenV2Contract/useAllowance";
+import useApprove from "../LpTokenV2Contract/useApprove";
 import useTotalStaked from "../MasterchefContract/useTotalStaked";
 import useUserInfo from "../MasterchefContract/useUserInfo";
 import usePendingLand from "../MasterchefContract/usePendingLand";
 import { AUTO_VAULT_V3_CONTRACT_ADDRESS, MASTERCHEF_CONTRACT_ADDRESS } from "../../../config/constants/environments";
 
-export default function useVaultBalanceManual(address: Address | undefined) {
-  const [transactionState, setTransactionState] = useState("")
+export default function useVaultBalanceManual(address: Address | undefined, updateStatus: Function) {
+  const { setScreenLoadingStatus } = useGlobalContext()
   const { deposit, data: depositTx } = useDeposit()
   const { withdraw, data: withdrawTx } = useWithdraw()
+  const { approve, data: approveTx } = useApprove()
   const { data: lpTokenV2Balance, refetch: refetchLpTokenV2 } = useBalanceOfLpTokenV2({ address }) as {
     data: BigNumberish,
     refetch: Function
@@ -34,6 +37,10 @@ export default function useVaultBalanceManual(address: Address | undefined) {
     hash: withdrawTx,
     chainId: bsc.id
   });
+  const { isSuccess: approveSuccess } = useWaitForTransactionReceipt({
+    hash: approveTx,
+    chainId: bsc.id
+  });
 
   useEffect(() => {
     try {
@@ -41,13 +48,19 @@ export default function useVaultBalanceManual(address: Address | undefined) {
         refetchLpTokenV2()
         refetchAllowanceOfLpTokenV2OfVault()
         refetchAllowanceOfLpTokenV2OfMasterChef()
-        setTransactionState("Deposit Transaction success")
+        setScreenLoadingStatus("Deposit Transaction success")
       } else {
-        setTransactionState("Transaction failed")
+        setScreenLoadingStatus("Transaction failed")
       }
     } catch (error) {
-      setTransactionState("Transaction failed")
+      setScreenLoadingStatus("Transaction failed")
       console.log(error)
+    }
+
+    return () => {
+      setTimeout(() => {
+        setScreenLoadingStatus("")
+      }, 1000);
     }
   }, [depositSuccess])
 
@@ -58,15 +71,43 @@ export default function useVaultBalanceManual(address: Address | undefined) {
         refetchTotalStaked()
         refetchUserInfo()
         refetchPendingLand()
-        setTransactionState("Withdraw Transaction success")
+        setScreenLoadingStatus("Withdraw Transaction success")
       } else {
-        setTransactionState("Transaction failed")
+        setScreenLoadingStatus("Transaction failed")
       }
     } catch (error) {
-      setTransactionState("Transaction failed")
+      setScreenLoadingStatus("Transaction failed")
       console.log(error)
     }
+
+    return () => {
+      setTimeout(() => {
+        setScreenLoadingStatus("")
+      }, 1000);
+    }
   }, [withdrawSuccess])
+
+  useEffect(() => {
+    try {
+      if (approveSuccess) {
+        refetchAllowanceOfLpTokenV2OfVault()
+        refetchAllowanceOfLpTokenV2OfMasterChef()
+
+        setScreenLoadingStatus("Approve Transaction success")
+      } else {
+        setScreenLoadingStatus("Transaction failed")
+      }
+    } catch (error) {
+      setScreenLoadingStatus("Transaction failed")
+      console.log(error)
+    }
+
+    return () => {
+      setTimeout(() => {
+        setScreenLoadingStatus("")
+      }, 1000);
+    }
+  }, [approveSuccess])
 
   const depositVault = (amount: BigNumberish) => {
     if (Number(formatEther(amount)) > Number(formatEther(lpTokenV2Balance))) {
@@ -74,16 +115,24 @@ export default function useVaultBalanceManual(address: Address | undefined) {
       return;
     }
 
+    setScreenLoadingStatus("Deposit Transaction in progress...")
     deposit(amount)
   }
 
   const withdrawVault = (amount: BigNumberish) => {
+    setScreenLoadingStatus("Withdraw Transaction in progress...")
     withdraw(amount)
+  }
+
+  const approveVault = () => {
+    setScreenLoadingStatus("Approve Transaction in progress...")
+    approve(MASTERCHEF_CONTRACT_ADDRESS, "1000000000000000000000000000000")
+    updateStatus()
   }
 
   return {
     depositVault,
     withdrawVault,
-    transactionState
+    approveVault
   }
 }
