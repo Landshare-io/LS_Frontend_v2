f9f6f14fca7fe16f2e7b164fdb6be7df34f26781
mogw <mogwinfo@gmail.com>
2024-10-01 15:57:26 +0100

    Updated LpTokenV2Contract's useTotalSupply hook

--- src/hooks/contract/LpTokenV2Contract/useTotalSupply.ts
+++ src/hooks/contract/LpTokenV2Contract/useTotalSupply.ts
@@ -4,18 +4,18 @@ import LpTokenV2Abi from "../../../abis/LpTokenV2.json";
 import { LP_TOKEN_V2_CONTRACT_ADDRESS } from "../../../config/constants/environments";
 
 export default function useTotalSupply() {
-  const { data, isError, isLoading, error } = useReadContract({
+  const { data, isError, isLoading, error, refetch } = useReadContract({
     address: LP_TOKEN_V2_CONTRACT_ADDRESS,
     abi: LpTokenV2Abi,
     functionName: "totalSupply",
     chainId: bsc.id
   })
 
-  if (isLoading) return 0
+  if (isLoading) return { data: 0, refetch }
   if (isError) {
     console.log('Fetching LpTokenV2Contract totalSupply error', error)
-    return 0
+    return { data: 0, refetch }
   }
 
-  return data
+  return { data, refetch }
 }
