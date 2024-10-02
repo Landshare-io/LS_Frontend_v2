1ebb67e3270bd407d8c3321471b70288493dd466
mogw <mogwinfo@gmail.com>
2024-10-02 15:49:25 +0100

    Added UsdtContract's useBalanceOf hook

---
+++ src/hooks/contract/UsdtContract/useBalanceOf.ts
@@ -0,0 +1,23 @@
+import { useReadContract } from "wagmi";
+import { bsc } from "viem/chains";
+import UsdtAbi from "../../../abis/USDT.json";
+import { USDT_ADDRESS } from "../../../config/constants/environments";
+import { Address } from "viem";
+
+export default function useBalanceOf(chainId: number, address: Address | undefined) {
+  const { data, isError, isLoading, error, refetch } = useReadContract({
+    address: USDT_ADDRESS[chainId],
+    abi: UsdtAbi,
+    functionName: "balanceOf",
+    chainId: bsc.id,
+    args: [address]
+  })
+
+  if (isLoading) return { data: 0, refetch }
+  if (isError) {
+    console.log('Fetching RWAContract balanceOf error', error)
+    return { data: 0, refetch }
+  }
+
+  return { data, refetch }
+}
