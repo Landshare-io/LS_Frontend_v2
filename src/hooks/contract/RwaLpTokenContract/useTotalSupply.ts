afd1ae3fc052153655c8b7573f788554b979ee25
mogw <mogwinfo@gmail.com>
2024-10-02 15:49:13 +0100

    Added RwaLpContract's useTotalSupply hook

---
+++ src/hooks/contract/RwaLpTokenContract/useTotalSupply.ts
@@ -0,0 +1,21 @@
+import { useReadContract } from "wagmi";
+import { bsc } from "viem/chains";
+import RwaLpContractAbi from "../../../abis/RwaLpContract.json";
+import { RWA_LP_CONTRACT_ADDRESS } from '../../../config/constants/environments';
+
+export default function useTotalSupply() {
+  const { data, isError, isLoading, error } = useReadContract({
+    address: RWA_LP_CONTRACT_ADDRESS,
+    abi: RwaLpContractAbi,
+    functionName: "totalSupply",
+    chainId: bsc.id
+  })
+
+  if (isLoading) return 0
+  if (isError) {
+    console.log('Fetching RWAContract totalSupply error', error)
+    return 0
+  }
+
+  return data
+}
