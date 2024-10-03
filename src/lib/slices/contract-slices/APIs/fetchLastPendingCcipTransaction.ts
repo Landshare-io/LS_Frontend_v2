c5dc06a8937437504406e7b45e63c1e1b9f9a80a
mogw <mogwinfo@gmail.com>
2024-10-03 16:11:29 +0100

    Added fetchLastPendingCcipTransaction API

---
+++ src/lib/slices/contract-slices/APIs/fetchLastPendingCcipTransaction.ts
@@ -0,0 +1,13 @@
+import { Address } from "viem";
+import { CCIP_BACKEND_URL } from "../../../../config/constants/environments";
+
+
+export const fetchLastPendingCcipTransaction = async (address: Address) => {
+  const response = await fetch(`${CCIP_BACKEND_URL}/ccip-transaction/get-last-pending-tx/${address}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  const { data } = await response.json();
+
+  return data;
+};
