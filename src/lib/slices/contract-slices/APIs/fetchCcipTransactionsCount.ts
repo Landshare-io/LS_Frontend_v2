db3fc2960e4ae859379229ecaff6949632d3f663
mogw <mogwinfo@gmail.com>
2024-10-03 16:12:39 +0100

    Added fetchCcipTransactionsCount API

---
+++ src/lib/slices/contract-slices/APIs/fetchCcipTransactionsCount.ts
@@ -0,0 +1,12 @@
+import { Address } from "viem";
+import { CCIP_BACKEND_URL } from "../../../../config/constants/environments";
+
+export const fetchCcipTransactionsCount = async (address: Address) => {
+  const response = await fetch(`${CCIP_BACKEND_URL}/ccip-transaction/count/${address}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  const { data } = await response.json();
+
+  return data;
+};
