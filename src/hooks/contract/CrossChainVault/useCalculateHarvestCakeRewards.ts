271f377550e9d143a16e563cca86a04ab02b9113
mogw <mogwinfo@gmail.com>
2024-10-04 16:48:14 +0100

    Added CrossChainVault contract's useCalculateHarvestCakeRewards hook

---
+++ src/hooks/contract/CrossChainVault/useCalculateHarvestCakeRewards.ts
@@ -0,0 +1,21 @@
+import { useReadContract } from "wagmi";
+import AutoVaultV2Contract from "../../../abis/AutoVaultV2.json"
+import { CCIP_CHAIN_VAULT_ADDRESS } from "../../../config/constants/environments";
+
+export default function useCalculateHarvestCakeRewards(chainId: number) {
+  const { data, isError, isLoading, error, refetch } = useReadContract({
+    address: CCIP_CHAIN_VAULT_ADDRESS[chainId],
+    abi: AutoVaultV2Contract,
+    functionName: "calculateHarvestCakeRewards",
+    args: [],
+    chainId: chainId
+  })
+
+  if (isLoading) return { data: 0, refetch, isLoading }
+  if (isError) {
+    console.log('Fetching cross chain vault getting calculateHarvestCakeRewards error', error)
+    return { data: 0, refetch, isLoading: false }
+  }
+
+  return { data, refetch, isLoading }
+}
