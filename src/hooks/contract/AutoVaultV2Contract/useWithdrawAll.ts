bdd3058806d6e86d4da386fc77b62defc7aac38f
mogw <mogwinfo@gmail.com>
2024-10-04 18:58:43 +0100

    Added AutoVaultV3Contract's useWithdrawAll hook

---
+++ src/hooks/contract/AutoVaultV2Contract/useWithdrawAll.ts
@@ -0,0 +1,31 @@
+import { useWriteContract } from "wagmi";
+import { bsc } from "viem/chains";
+import type { Address } from "viem";
+import AutoVaultV2Contract from "../../../abis/AutoVaultV2.json"
+import { AUTO_VAULT_V3_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+import { BigNumberish } from "ethers";
+
+
+export default function useWithdrawAll() {
+  const {
+    data,
+    isPending,
+    writeContract
+  } = useWriteContract();
+
+  async function withdrawAll() {
+    await writeContract({
+      address: AUTO_VAULT_V3_CONTRACT_ADDRESS as Address,
+      abi: AutoVaultV2Contract,
+      functionName: "withdrawAll",
+      chainId: bsc.id,
+      args: []
+    });
+  }
+
+  return {
+    withdrawAll,
+    isPending,
+    data
+  }
+}
