723476c4f8714ca5d0f638cfdd862e25beac3c75
mogw <mogwinfo@gmail.com>
2024-10-04 03:45:31 +0100

    Added AutoVaultV3Contract's useTotalShares hook

---
+++ src/hooks/contract/AutoVaultV2Contract/useTotalShares.ts
@@ -0,0 +1,23 @@
+import { useReadContract } from "wagmi";
+import { bsc } from "viem/chains";
+import { Address } from "viem";
+import AutoVaultV2Contract from "../../../abis/AutoVaultV2.json"
+import { AUTO_VAULT_V3_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+
+export default function useTotalShares() {
+  const { data, isError, isLoading, error, refetch } = useReadContract({
+    address: AUTO_VAULT_V3_CONTRACT_ADDRESS,
+    abi: AutoVaultV2Contract,
+    functionName: "totalShares",
+    args: [],
+    chainId: bsc.id
+  })
+
+  if (isLoading) return { data: [0, 0], refetch, isLoading }
+  if (isError) {
+    console.log('Fetching autoVaultv3 getting totalShares error', error)
+    return { data: [0, 0], refetch, isLoading: false }
+  }
+
+  return { data, refetch, isLoading }
+}
