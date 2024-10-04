bf817a389c2e31d092684c68a83393f4a990c066
mogw <mogwinfo@gmail.com>
2024-10-04 19:00:01 +0100

    Updated auto vault component

--- src/lib/slices/contract-slices/APIs/updateCcipTransaction.ts
+++ src/lib/slices/contract-slices/APIs/updateCcipTransaction.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
+import { CCIP_BACKEND_URL } from "../../../../config/constants/environments"
 
 export const updateCcipTransaction = async (data: any) => {
-  const { data: transactionData } = await axios.post('/ccip-transaction', data)
+  const { data: transactionData } = await axios.post(`${CCIP_BACKEND_URL}/ccip-transaction`, data)
 
   return transactionData
 }
