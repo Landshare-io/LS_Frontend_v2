9d85015746ea07f2fc421dbc59e39ccebcbc18f1
mogw <mogwinfo@gmail.com>
2024-10-04 16:48:27 +0100

    Added CrossChainVault contract's useCcipVaultBalance hook

---
+++ src/hooks/contract/CrossChainVault/useCcipVaultBalance.ts
@@ -0,0 +1,23 @@
+import { Address } from "viem";
+import useTotal from "./useTotal";
+import useTotalShares from "./useTotalShares";
+import useUserInfo from "./useUserInfo";
+import { BigNumberish } from "ethers";
+
+export default function useCcipVaultBalance(chainId: number, address: Address | undefined) {
+  const { data: total } = useTotal(chainId) as { data: BigNumberish }
+  const { data: totalShares } = useTotalShares(chainId) as { data: BigNumberish }
+  const { data: userInfo } = useUserInfo(chainId, address) as { data: [BigNumberish, BigNumberish, BigNumberish, BigNumberish] }
+
+  const autoLandV3 = totalShares ? BigInt(userInfo[0]) * BigInt(total) / BigInt(totalShares) : 0
+  const autoReward = totalShares ? 
+    (BigInt(userInfo[0]) * BigInt(total) / BigInt(totalShares) - BigInt(userInfo[2]) > 0 ? (BigInt(userInfo[0]) * BigInt(total) / BigInt(totalShares) - BigInt(userInfo[2])) : 0)
+    : 0
+
+  return {
+    total,
+    totalSharesV3: totalShares,
+    autoLandV3,
+    autoReward
+  }
+}
