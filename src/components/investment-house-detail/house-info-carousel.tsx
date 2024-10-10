e16c1a22cec95c543a253a57330d31d528e433fe
mogw <mogwinfo@gmail.com>
2024-10-10 15:05:05 +0100

    Added house info carousel component

---
+++ src/components/investment-house-detail/house-info-carousel.tsx
@@ -0,0 +1,65 @@
+import React, { useMemo } from "react";
+import { Carousel } from "react-responsive-carousel";
+import CircleLoader from "../common/circle-loader";
+import { 
+  getData,
+  selectIsLoading,
+  selectPropertyRentalData
+} from "../../lib/slices/firebase-slices/properties-rental-item";
+import { useAppDispatch, useAppSelector } from "../../lib/hooks";
+import { BOLD_INTER_TIGHT } from "../../config/constants/environments";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import styles from "./investment-house-detail.module.css";
+
+interface HouseInfoCarouselComponentProps {
+  itemId: string
+}
+
+export default function HouseInfoCarouselComponent({ itemId }: HouseInfoCarouselComponentProps) {
+  const dispatch = useAppDispatch();
+  const houseInfo = useAppSelector(selectPropertyRentalData) as any;
+  const isLoading = useAppSelector(selectIsLoading)
+
+  useMemo(async () => {
+    dispatch(getData(itemId))
+  }, [itemId]);
+
+  return (
+    <>
+      {houseInfo?.address ? (
+        <div className="max-w-[1200px] m-auto my-[10px] py-[10px]">
+          <div className="flex mt-[20px] gap-[40px]">
+            <div className="w-full">
+              {isLoading ? (
+                <div className="flex justify-center items-center h-100 min-h-[420px]">
+                  <CircleLoader />
+                </div>
+              ) : (
+                <div className={styles.carouselContainer}>
+                  <Carousel>
+                    {houseInfo?.pictures?.map((imgObj: any) =>
+                      <div>
+                        <img className="rounded-[13px]" src={imgObj} alt="" />
+                      </div>
+                    )}
+                  </Carousel>
+                </div>
+              )}
+            </div>
+            <div className="flex flex-col w-full">
+              <div className="flex gap-[15px] text-[16px] text-text-secondary">
+                <span>Home</span>
+                <span>{houseInfo?.address}</span>
+              </div>
+              <h1 className={`pt-[10px] text-[20px] md:text-[30px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>
+                {houseInfo?.address}
+              </h1>
+              <div className="flex flex-col gap-[10px] text-[16px] tracking-[0.02em] pt-[10px] pb-[5px] text-text-secondary" dangerouslySetInnerHTML={{ __html: houseInfo?.description }}>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
