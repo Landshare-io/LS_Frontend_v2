a5a51f37b83bac9d33a8b46abb7e92c59c16ad77
mogw <mogwinfo@gmail.com>
2024-10-11 19:55:38 +0100

    Added dao proposal content burn token component

---
+++ src/components/dao-proposal-content/burn-token.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import ReactLoading from "react-loading";
+import Input from "../common/input";
+import { BOLD_INTER_TIGHT } from "../../config/constants/environments";
+
+export default function BurnTokens(props) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  return isLoading ? (
+    <div className="flex w-full justify-center my-auto">
+      <ReactLoading type="cylon" color="#61cd81" />
+    </div>
+  ) : (
+    <div className="text-[16px] shadow-lg shadow-[#0003] bg-third">
+      <div className={`p-[10px] text-[18px] bg-[#87D99F] text-white text-button-text-secondary ${BOLD_INTER_TIGHT.className}`}>Burn DAO Treasury Tokens</div>
+      <div className="my-[10px] mx-[7px]">
+        <Input
+          proposal="Burn Tokens"
+          error={props.error}
+          setError={props.setError}
+          value={props.amountToBurn}
+          setValue={props.setAmountToBurn}
+          labelClassName="my-3 text-text-secondary"
+          containerClassName="w-full"
+          label="Enter the amount of tokens to burn. All token burns are sent to the null address and lower the total supply cap."
+          max={props.balance}
+        />
+        <div className="text-[13px] text-right mr-[5px] mt-[2px] mb-[2px] text-text-secondary">
+          Available Balance:{" "}
+          <b>{props.balance.match(/^-?\d+(?:\.\d{0,4})?/)[0]} LAND</b>
+        </div>
+      </div>
+    </div>
+  );
+};
