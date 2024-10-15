9c1f099683e0d2922a3f410c98b825585ac53ce4
mogw <mogwinfo@gmail.com>
2024-10-15 16:34:53 +0100

    Added auto vault v2 contract's usePerformanceFee hook

---
+++ src/hooks/contract/AutoVaultV2Contract/usePerformanceFee.ts
@@ -0,0 +1,24 @@
+import { useReadContract } from "wagmi";
+import { Address } from "viem";
+import { bsc } from "viem/chains";
+import AutoVaultV2Contract from "../../../abis/AutoVaultV2.json"
+import { AUTO_VAULT_V3_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+
+
+export default function usePerformanceFee( chainId: number) {
+  const { data, isError, isLoading, error, refetch } = useReadContract({
+    address: AUTO_VAULT_V3_CONTRACT_ADDRESS[bsc.id],
+    abi: AutoVaultV2Contract,
+    functionName: "performanceFee",
+    args: [],
+    chainId: bsc.id
+  })
+
+  if (isLoading) return { data: 0, refetch, isLoading }
+  if (isError) {
+    console.log('Fetching autoVaultv3 userInfo error', error)
+    return { data: 0, refetch, isLoading: false }
+  }
+
+  return { data, refetch, isLoading }
+}
