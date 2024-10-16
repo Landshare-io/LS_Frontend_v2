1c3517a21bf30276dc8e85b982c8a49f343f1221
mogw <mogwinfo@gmail.com>
2024-10-16 17:36:51 +0100

    Updated custom proposal component

--- src/components/dao-proposal-content/custom-proposal.tsx
+++ src/components/dao-proposal-content/custom-proposal.tsx
@@ -3,7 +3,7 @@ import { BOLD_INTER_TIGHT } from "../../config/constants/environments";
 export default function CustomProposal() {
   return (
     <>
-      <div className="text-[16px] shadow-lg shadow-[#0003] bg-third">
+      <div className="my-[25px] text-[16px] shadow-lg shadow-[#0003] bg-third">
         <div className={`p-[10px] text-[18px] bg-[#61CD81] text-button-text-secondary ${BOLD_INTER_TIGHT.className}`}>Custom proposal</div>
 
         <div className="my-[10px] mx-[7px] text-[15px] text-text-secondary">
