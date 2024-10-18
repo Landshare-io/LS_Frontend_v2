853f6460323a8303c161fb03443eb7454ea450d2
mogw <mogwinfo@gmail.com>
2024-10-18 16:14:45 +0100

    Added AutoLandV2Contract's useWithdraw hook

---
+++ src/hooks/contract/AutoLandV2Contract/useWithdraw.ts
@@ -0,0 +1,30 @@
+import { useWriteContract } from "wagmi";
+import { BigNumberish } from "ethers";
+import { bsc } from "viem/chains";
+import AutoLandVaultV2 from '../../../abis/AutoLandVaultV2.json';
+import { AUTO_LAND_V2_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+
+
+export default function useWithdraw() {
+  const {
+    data,
+    isPending,
+    writeContract
+  } = useWriteContract();
+
+  async function withdraw(amount: BigNumberish) {
+    await writeContract({
+      address: AUTO_LAND_V2_CONTRACT_ADDRESS,
+      abi: AutoLandVaultV2,
+      functionName: "withdraw",
+      chainId: bsc.id,
+      args: [amount]
+    });
+  }
+
+  return {
+    withdraw,
+    isPending,
+    data
+  }
+}
