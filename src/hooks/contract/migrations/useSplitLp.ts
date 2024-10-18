3bd055af1ef1a2a3048c0f0fc6bfe8ce531d64fc
mogw <mogwinfo@gmail.com>
2024-10-18 10:14:42 +0100

    Updated useSplitLp hook

--- src/hooks/contract/migrations/useSplitLp.ts
+++ src/hooks/contract/migrations/useSplitLp.ts
@@ -1,13 +1,19 @@
-import { useState } from "react";
-import { useAccount } from "wagmi";
+import { useState, useEffect } from "react";
+import { 
+  useAccount,
+  useWaitForTransactionReceipt
+} from "wagmi";
 import { Address } from "viem";
+import { bsc } from "viem/chains";
 import useBalanceOf from "../LpTokenV1Contract/useBalanceOf";
 import useApprove from "../LpTokenV1Contract/useApprove";
 import useRemoveLiquidityETH from "../PCSRouterContract/useRemoveLiquidityETH";
-import { useLandshareV1Context } from "../contexts/LandshareV1Context";
-import { useMigrationContext } from "../contexts/MigrationContext";
 import { useGlobalContext } from "../../../context/GlobalContext";
-import { LP_TOKEN_V1_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+import { 
+  PROVIDERS,
+  LAND_TOKEN_V1_CONTRACT_ADDRESS,
+  PSC_ROUTER_CONTRACT_ADDRESS 
+} from "../../../config/constants/environments";
 
 interface useSplitLPProps {
   address: Address | undefined
@@ -17,67 +23,99 @@ export default function useSplitLP({
   address
 }: useSplitLPProps) {
   const [isSuccessSplit, setIsSuccessSplit] = useState(false);
+  const [removeLiquidityETHAmount, setRemoveLiquidityETHAmount] = useState<string | number>(0)
+  const [removeLiquidityETHMinLand, setRemoveLiquidityETHMinLand] = useState<string | number | bigint>(0)
+  const [removeLiquidityETHMinEth, setRemoveLiquidityETHMinEth] = useState<string | number | bigint>(0)
   const [amountSplitedTokens, setAmountSplitedTokens] = useState({
     bnb: 0,
     land: 0,
   });
   const {setScreenLoadingStatus} = useGlobalContext()
   const { isConnected } = useAccount();
-  const {
-    contract: { lpTokenContract },
-  } = useLandshareV1Context();
-  const { PCSRouterContract } = useMigrationContext();
   const { data: balance } = useBalanceOf({ address })
-  const { approve } = useApprove()
+  const { approve, data: approveTx } = useApprove()
+  const { removeLiquidityETH, data: removeLiquidityETHTx } = useRemoveLiquidityETH()
+
+  const { isSuccess: approveSuccess } = useWaitForTransactionReceipt({
+    hash: approveTx,
+    chainId: bsc.id
+  });
+
+  const { isSuccess: removeLiquiditySuccess } = useWaitForTransactionReceipt({
+    hash: removeLiquidityETHTx,
+    chainId: bsc.id
+  });
+
+  useEffect(() => {
+    if (approveTx) {
+      if (approveSuccess) {
+        try {
+          setScreenLoadingStatus("Transaction 2 of 2 Pending...")
+          removeLiquidityETH(
+            LAND_TOKEN_V1_CONTRACT_ADDRESS,
+            removeLiquidityETHAmount, removeLiquidityETHMinLand, removeLiquidityETHMinEth,
+            address,
+            Date.now()
+          )
+        } catch (error) {
+          console.log("swap error", error)
+          setScreenLoadingStatus("Transaction Failed.")
+
+          return () => {
+            setTimeout(() => {
+              setScreenLoadingStatus("")
+            }, 1000);
+          }
+        }
+      }
+    }
+  }, [approveSuccess, approveTx])
+
+  useEffect(() => {
+    (async () => {
+      if (removeLiquidityETHTx) {
+        if (removeLiquiditySuccess) {
+          try {
+            const receiptTx = await PROVIDERS[bsc.id].getTransactionReceipt(removeLiquidityETHTx);
+            const amountLand = receiptTx.args.amount0;
+            const amountBNB = receiptTx.args.amount1;
+            setAmountSplitedTokens({ bnb: amountBNB, land: amountLand });
+            setScreenLoadingStatus("Transaction Completed.")
+            setIsSuccessSplit(true)
+          } catch (error) {
+            setScreenLoadingStatus("Transaction Failed.")
+          }
+          return () => {
+            setTimeout(() => {
+              setScreenLoadingStatus("")
+            }, 1000);
+          }
+        }
+      }
+    })()
+  }, [removeLiquiditySuccess, removeLiquidityETHTx])
 
   async function splitLP(amount: string | number, minLand: string | number | bigint, minEth: string | number | bigint) {
     if (isConnected == true) {
+      setRemoveLiquidityETHAmount(amount)
+      setRemoveLiquidityETHMinLand(minLand)
+      setRemoveLiquidityETHMinEth(minEth)
       if (Number(amount) > Number(balance)) {
         window.alert("Insufficient Balance");
         return;
       }
       try {
-        const txApprove = await lpTokenContract.approve(
-          process.env.REACT_APP_PSC_ROUTER,
-          amount
-        );
-        txApprove.wait().then(async () => {
-          
-          try {
-            const txRemoveLiquidity = await PCSRouterContract.removeLiquidityETH(
-              process.env.REACT_APP_LAND_TOKEN_ADDR,
-              amount,
-              minLand,
-              minEth,
-              address,
-              Date.now()
-            );
-            txRemoveLiquidity.wait().then( (res) => {
-              const removeLiquidityEvent = res.events.find(event => event.event === 'Burn');
-              const amountLand = removeLiquidityEvent.args.amount0;
-              const amountBNB = removeLiquidityEvent.args.amount1;
-              setAmountSplitedTokens({ bnb: amountBNB, land: amountLand });
-              transactionResult("Transaction Completed.", true);
-              setIsSuccessSplit(true);
-         
-            });
-          } catch (e) {
-            transactionResult("Transaction Failed.", false);
-            setIsSuccessSplit(false);
-            console.log("Error, withdraw: ", e);
-          }
-     
-        });
-
+        setScreenLoadingStatus("Transaction 1 of 2 Pending...")
+        approve(PSC_ROUTER_CONTRACT_ADDRESS, amount)
+        
       } catch (e) {
-        transactionResult("Transaction Failed.", false);
+        setScreenLoadingStatus("Transaction failed")
         setIsSuccessSplit(false);
         console.log("Error, withdraw: ", e);
       }
    
-    } else {
-      endTransaction();
     }
   }
+
   return { splitLP, isSuccessSplit, amountSplitedTokens, setIsSuccessSplit };
 }
