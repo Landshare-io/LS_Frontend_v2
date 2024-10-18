3fecd54d84247c6c45dfb7c9a4a4c3e140d1b534
mogw <mogwinfo@gmail.com>
2024-10-18 16:15:41 +0100

    Added LandLPFarmContract's useWithdraw hook

---
+++ src/hooks/contract/LandLPFarmContract/useWithdraw.ts
@@ -0,0 +1,30 @@
+import { useWriteContract } from "wagmi";
+import { BigNumberish } from "ethers";
+import { bsc } from "viem/chains";
+import LandLPFarm from '../../../abis/LandLPFarm.json';
+import { LP_FARM_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+
+
+export default function useWithdraw() {
+  const {
+    data,
+    isPending,
+    writeContract
+  } = useWriteContract();
+
+  async function withdraw(amount: BigNumberish) {
+    await writeContract({
+      address: LP_FARM_CONTRACT_ADDRESS,
+      abi: LandLPFarm,
+      functionName: "withdraw",
+      chainId: bsc.id,
+      args: [amount]
+    });
+  }
+
+  return {
+    withdraw,
+    isPending,
+    data
+  }
+}
