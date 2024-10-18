82709c323f1c448ba101463cd9f428dd17d35dc7
mogw <mogwinfo@gmail.com>
2024-10-18 04:18:39 +0100

    Added PCSRouterContract's useRemoveLiquidityETH hook

---
+++ src/hooks/contract/PCSRouterContract/useRemoveLiquidityETH.ts
@@ -0,0 +1,30 @@
+import { useWriteContract } from 'wagmi'
+import { Address } from 'viem';
+import { BigNumberish } from 'ethers';
+import { bsc } from 'viem/chains';
+import PscRouter from "../../../abis/PancakeRouter.json";
+import { PSC_ROUTER_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+
+export default function useRemoveLiquidityETH() {
+  const {
+    data,
+    isPending,
+    writeContract
+  } = useWriteContract();
+
+  async function removeLiquidityETH(tokenAddress: Address, liquidity: number | BigNumberish, amountTokenMin: number | BigNumberish, amountETHMin: number | BigNumberish, to: Address, deadline: number | BigNumberish) {
+    await writeContract({
+      address: PSC_ROUTER_CONTRACT_ADDRESS,
+      abi: PscRouter,
+      functionName: "removeLiquidityETH",
+      args: [tokenAddress, liquidity, amountTokenMin, amountETHMin, to, deadline],
+      chainId: bsc.id
+    });
+  }
+
+  return {
+    removeLiquidityETH,
+    isPending,
+    data
+  }
+}
