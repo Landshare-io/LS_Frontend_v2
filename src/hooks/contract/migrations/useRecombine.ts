98d6d599fd691f4141303f0d40b72a7e7fe5f522
mogw <mogwinfo@gmail.com>
2024-10-21 11:56:02 +0100

    Added migration's useRecombine hook

---
+++ src/hooks/contract/migrations/useRecombine.ts
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react";
+import { bsc } from "viem/chains";
+import { Address } from "viem";
+import { 
+  useBalance, 
+  useAccount,
+  useWaitForTransactionReceipt
+} from "wagmi";
+import { useGlobalContext } from "../../../context/GlobalContext";
+import useBalanceOfLandToken from "../LandTokenContract/useBalanceOf";
+import useAllowanceOfLandToken from "../LandTokenContract/useAllowance";
+import useApproveLandToken from "../LandTokenContract/useApprove";
+import useAddLiquidityETH from "../PCSRouterContract/useAddLiquidityETH";
+import { PROVIDERS, PSC_ROUTER_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+import { BigNumberish } from "ethers";
+
+interface useRecombineProps {
+  address: Address | undefined
+}
+
+export default function useRecombine({
+  address,
+}: useRecombineProps) {
+  const { setScreenLoadingStatus } = useGlobalContext()
+  const { isConnected } = useAccount();
+  const provider = PROVIDERS[bsc.id]
+  const [isSuccessRecombine, setIsSuccessRecombine] = useState(false);
+  const [LandAmount, setLandAmount] = useState<number | string | BigNumberish>(0)
+  const [minGas, setMinGas] = useState<number | string | BigNumberish>(0)
+
+  const { data: balanceBNB } = useBalance({ chainId: bsc.id, address }) as { data: BigNumberish }
+  const { data: balanceLandV2 } = useBalanceOfLandToken({ chainId: bsc.id, address }) as { data: BigNumberish }
+  const { data: landAllowance } = useAllowanceOfLandToken(bsc.id, address, PSC_ROUTER_CONTRACT_ADDRESS) as { data: BigNumberish }
+  const { approve, data: approveTx } = useApproveLandToken()
+  const { isSuccess: approveSuccess } = useWaitForTransactionReceipt({
+    hash: approveTx,
+    chainId: bsc.id
+  });
+
+  const { addLiquidityETH, data: addLiquidityETHTx } = useAddLiquidityETH()
+  const { isSuccess: addLiquiditySuccess } = useWaitForTransactionReceipt({
+    hash: addLiquidityETHTx,
+    chainId: bsc.id
+  })
+
+  useEffect(() => {
+    if (approveTx) {
+      if (approveSuccess) {
+        setScreenLoadingStatus("Transaction 2 of 2 Pending...")
+        addLiquidityETH(LandAmount, address, minGas)
+      } else {
+        setScreenLoadingStatus("Transaction Failed")
+        setIsSuccessRecombine(false);
+
+        return () => {
+          setTimeout(() => {
+            setScreenLoadingStatus("")
+          }, 1000);
+        }
+      }
+    }
+  }, [approveTx, approveSuccess])
+
+  useEffect(() => {
+    if (addLiquidityETHTx) {
+      if (addLiquiditySuccess) {
+        setScreenLoadingStatus("Transaction success")
+        setIsSuccessRecombine(true);
+      } else {
+        setScreenLoadingStatus("Transaction Failed")
+        setIsSuccessRecombine(false);
+      }
+
+      return () => {
+        setTimeout(() => {
+          setScreenLoadingStatus("")
+        }, 1000);
+      }
+    }
+  }, [addLiquidityETHTx, addLiquiditySuccess])
+
+  async function recombine(
+    amountBNB: string | number | BigNumberish,
+    amountLandV2: string | number | BigNumberish,
+    // minLand,
+    minEth: string | number | BigNumberish
+  ) {
+    if (isConnected == true) {
+      setLandAmount(amountLandV2)
+      setMinGas(minEth)
+      if (
+        Number(amountBNB) > Number(balanceBNB) ||
+        Number(amountLandV2) > Number(balanceLandV2)
+      ) {
+        window.alert("Insufficient BNB balance. ");
+        return;
+      }
+      try {
+        if (Number(landAllowance) < Number(amountLandV2)) {
+          setScreenLoadingStatus("Transaction 2 of 1 Pending...")
+          approve(bsc.id, PSC_ROUTER_CONTRACT_ADDRESS, amountLandV2);
+        } else {
+          setScreenLoadingStatus("")
+        }
+      } catch (e) {
+        setScreenLoadingStatus("Transaction Failed.");
+        setIsSuccessRecombine(false);
+        console.log("Error, withdraw: ", e);
+      }
+    }
+  }
+  return { recombine, isSuccessRecombine, setIsSuccessRecombine };
+}
