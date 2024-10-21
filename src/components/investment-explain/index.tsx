8b64ce41ff98ef056226eda04c7899e3061db22f
mogw <mogwinfo@gmail.com>
2024-10-21 17:26:15 +0100

    Updated rwa page

--- src/components/investment-explain/index.tsx
+++ src/components/investment-explain/index.tsx
@@ -4,14 +4,32 @@ import form from "../../../public/verify-steps/form.svg";
 import swapToken from "../../../public/verify-steps/swap-token.svg";
 import growthTime from "../../../public/verify-steps/growth-time.svg";
 import content from "../../../public/verify-steps/content.svg";
+import arrowLeft from "../../../public/icons/arrow-left.svg";
 
 export default function InvestmentExplain() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
   return (
     <div className="flex flex-col gap-[40px] items-center p-0 mlg:px-[20px] md:pt-[40px] xl:px-0 xl:pb-[80px] max-w-[1200px] m-auto">
-      <h2 className={`text-[46px] text-text-primary text-center leading-[59px] mb-[20px] ${BOLD_INTER_TIGHT.className}`}>Effortless Property Investment in 3 Steps</h2>
-      <div className="grid grid-cols-1 md:grid-cols-[minmax(270px,max-content),minmax(270px,max-content)] mlg:grid-cols-[minmax(270px,max-content),minmax(270px,max-content),minmax(270px,max-content)] w-full justify-between gap-y-[20px]">
+      <div className="flex flex-col gap-[10px] mb-[20px] items-center">
+        <h2
+          id="target-element"
+          className={`text-[46px] text-tw-text-primary text-center leading-[59px] font-bold`}
+        >
+          Effortless Property Investment in 3 Steps
+        </h2>
+        <p className="text-[14px] leading-[17px] max-w-[719px] text-center">
+          Unlock the world of property investment with ease through Landshare and RWA tokens. Our simple, 3-step process lets you invest in real estate without the complexities of traditional methods.
+        </p>
+      </div>
+      <div className="flex flex-col items-center mlg:flex-row w-full gap-[10px]">
         {/* Step 1 */}
-        <div className="flex flex-col w-full md:max-w-[270px] h-[354px] items-center justify-between p-[20px] bg-gradient-to-b from-[#61CD81] to-[#2B9F49] rounded-[24px] overflow-hidden relative">
+        <div className="flex flex-col shrink-0 grow-0 w-full md:w-[270px] h-[354px] items-center justify-between p-[20px] bg-gradient-to-b from-[#61CD81] to-[#2B9F49] rounded-[24px] overflow-hidden relative">
           <div className="flex justify-center gap-[7px]">
             <div className="flex justify-center items-center border border-[#fff] w-[21px] h-[21px] rounded-full">
               <span className={`text-[14px] leading-[10px] rounded-full text-[#fff] ${BOLD_INTER_TIGHT.className}`}>1</span>
@@ -19,11 +37,22 @@ export default function InvestmentExplain() {
             <span className={`text-[20px] leading-[22px] text-[#fff] ${BOLD_INTER_TIGHT.className}`}>Sign up</span>
           </div>
           <Image src={form} alt="form" className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]" />
-          <p className="text-[14px] leading-[21px] font-medium text-[#fff]">Sign up on the Landshare Dashboard and complete KYC</p>
+          <a
+            className="!text-[#000] w-full"
+            href="https://dashboard.landshare.io/"
+          >
+            <button className="!bg-[#fff] hover:bg-slate-200 transition-all hover:-translate-y-1 w-full rounded-[100px] py-[10px] text-[16px]  leading-[24px] font-bold">
+              Verify now
+            </button>
+          </a>
+        </div>
+
+        <div className="flex items-center justify-center w-full h-[100px] mlg:h-full">
+          <Image src={arrowLeft} alt="arrow-left" className="rotate-90 w-fit mlg:rotate-0 mlg:w-full h-[10px] text-[#2A8442]" />
         </div>
         
         {/* Step 2 */}
-        <div className="flex flex-col w-full md:max-w-[270px] h-[354px] items-center justify-between p-[20px] bg-gradient-to-b from-[#61CD81] to-[#2B9F49] rounded-[24px] overflow-hidden relative">
+        <div className="flex flex-col shrink-0 grow-0 w-full md:w-[270px] h-[354px] items-center justify-between p-[20px] bg-gradient-to-b from-[#61CD81] to-[#2B9F49] rounded-[24px] overflow-hidden relative">
           <div className="flex justify-center gap-[7px]">
             <div className="flex justify-center items-center border border-[#fff] w-[21px] h-[21px] rounded-full">
               <span className={`text-[14px] leading-[10px] rounded-full text-[#fff] ${BOLD_INTER_TIGHT.className}`}>2</span>
@@ -31,11 +60,20 @@ export default function InvestmentExplain() {
             <span className={`text-[20px] leading-[22px] text-[#fff] ${BOLD_INTER_TIGHT.className}`}>Purchase Tokens</span>
           </div>
           <Image src={swapToken} alt="swapToken" className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]" />
-          <p className="text-[14px] leading-[21px] font-medium text-[#fff]">Purchase RWA Tokens with LAND and USDC</p>
+          <button
+            onClick={() => scrollToTop()}
+            className="!bg-[#fff] hover:bg-slate-200 transition-all hover:-translate-y-1 w-full rounded-[100px] py-[10px] text-[16px]  leading-[24px] font-bold"
+          >
+            View details
+          </button>
+        </div>
+
+        <div className="flex items-center justify-center w-full h-[100px] mlg:h-full">
+          <Image src={arrowLeft} alt="arrow-left" className="rotate-90 w-fit mlg:rotate-0 mlg:w-full h-[10px] text-[#2A8442]" />
         </div>
         
         {/* Step 3 */}
-        <div className="flex flex-col w-full md:max-w-[270px] h-[354px] items-center justify-between p-[20px] bg-gradient-to-b from-[#61CD81] to-[#2B9F49] rounded-[24px] overflow-hidden relative">
+        <div className="flex flex-col shrink-0 grow-0 w-full md:w-[270px] h-[354px] items-center justify-between p-[20px] bg-gradient-to-b from-[#61CD81] to-[#2B9F49] rounded-[24px] overflow-hidden relative">
           <div className="flex justify-center gap-[7px]">
             <div className="flex justify-center items-center border border-[#fff] w-[21px] h-[21px] rounded-full">
               <span className={`text-[14px] leading-[10px] rounded-full text-[#fff] ${BOLD_INTER_TIGHT.className}`}>3</span>
@@ -43,7 +81,14 @@ export default function InvestmentExplain() {
             <span className={`text-[20px] leading-[22px] text-[#fff] ${BOLD_INTER_TIGHT.className}`}>Hold & Earn</span>
           </div>
           <Image src={growthTime} alt="growthTime" className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]" />
-          <p className="text-[14px] leading-[21px] font-medium text-[#fff]">Hold your tokens and see them grow over time</p>
+          <a
+            className="w-full !text-[#000]"
+            href=" https://docs.landshare.io/platform-features/landshare-rwa-token-lsrwa/rwa-token-utilities"
+          >
+            <button className="!bg-[#fff] hover:bg-slate-200 transition-all hover:-translate-y-1 w-full rounded-[100px] py-[10px] text-[16px]  leading-[24px] font-bold">
+              Learn more
+            </button>
+          </a>
         </div>
         
         {/* Step 4 */}
--- src/components/rwa-calculator/index.tsx
+++ src/components/rwa-calculator/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useChainId } from 'wagmi';
 import { BigNumberish, formatEther } from 'ethers';
+import { IoIosTrendingUp } from "react-icons/io";
 import Logo from '../common/logo';
 import Button from '../common/button';
 import {
@@ -24,59 +25,90 @@ export default function RwaCalculator() {
     dispatch(getData())
   }, [dispatch])
 
+  const scrollToElement = () => {
+    const element = document.querySelector("#target-element");
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
+  };
 
   return (
-    <div className="flex flex-col lg:flex-row gap-0 items-center p-0 max-w-[1200px] m-auto border-[2px] border-[#61CD81] rounded-[24px] overflow-hidden ">
-      <div className="flex flex-col flex-1 py-[34px] gap-[20px] justify-between px-[30px] h-[238px]">
-        <h2 className="text-[18px] leading-[24px] text-text-primary">Calculate returns effortlessly and make informed financial decisions for a brighter future.</h2>        
-        <div className='grid grid-cols-1 md:grid-cols-[minmax(200px,max-content),minmax(310px,max-content),minmax(50px,max-content)] gap-[20px] gap-y-[10px]'>
-          <span className={`text-[14px] leading-[22px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>Initial investment</span>
-          <input 
-            type="range" 
-            min="1000" 
-            max="10000" 
-            value={initialInvestment} 
-            onChange={(e) => setInitialInvestment(Number(e.target.value))} 
-            className="w-full max-w-auto md:max-w-[310px] accent-[#61CD81]"
-          />
-          <span className={`text-[18px] leading-[22px] text-[#61CD81] ${BOLD_INTER_TIGHT.className}`}>${initialInvestment.toLocaleString()}</span>
-          {/* <span className={`text-[14px] leading-[22px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>Monthly recurring investment</span>
-          <input 
-            type="range" 
-            min="100" 
-            max="5000" 
-            value={monthlyInvestment} 
-            onChange={(e) => setMonthlyInvestment(Number(e.target.value))} 
-            className="w-full max-w-[310px] accent-[#61CD81]"
-          />
-          <span className={`text-[18px] leading-[22px] text-[#61CD81] ${BOLD_INTER_TIGHT.className}`}>${monthlyInvestment.toLocaleString()}</span> */}
+    <div className='flex flex-col p-0 max-w-[1200px] m-auto gap-[50px]'>
+      <div className="flex items-center w-fit py-[6px] pr-[15px] pl-[6px] gap-[8px] h-[44px] rounded-[50px] text-[14px] font-medium leading-[22px] bg-secondary">
+        <div className="flex items-start p-[4px] w-[32px] h-[32px] rounded-[30px] bg-primary">
+          <IoIosTrendingUp className="w-[24px] h-[24px] text-[#24BC48]" />
         </div>
-        <div className="flex justify-between lg:justify-start items-center gap-[20px] text-text-primary">
-          <div className='flex flex-col items-center'>
-            <p className="text-[12px] leading-[22px] text-center">Annual rent return</p>
-            <p className={`text-[18px] leading-[22px] ${BOLD_INTER_TIGHT.className}`}>{(netRentalPerMonth * 12 / Number(formatEther(totalPropertyValue)) * 100).toFixed(3)} %</p>
-          </div>
-          <div className='flex flex-col items-center'>
-            <p className="text-[12px] leading-[22px] text-center">Annual value growth</p>
-            <p className={`text-[18px] leading-[22px] ${BOLD_INTER_TIGHT.className}`}>{(appreciation / Number(formatEther(totalPropertyValue))).toFixed(3)}%</p>
-          </div>
-          <div className='flex flex-col items-center'>
-            <p className="text-[12px] leading-[22px] text-center">Total annual return</p>
-            <p className={`text-[18px] leading-[22px] ${BOLD_INTER_TIGHT.className}`}>{(netRentalPerMonth * 12 / Number(formatEther(totalPropertyValue)) * 100 + appreciation / Number(formatEther(totalPropertyValue))).toFixed(3)}%</p>
-          </div>
-        </div>
+        <span className="text-[14px] leading-[22px] tracking-[0.02em] font-semibold text-text-primary">Estimate your potential earnings</span>
       </div>
-
-      <div className='flex flex-col w-full max-w-auto bg-[#61CD81] lg:max-w-[400px] overflow-hidden py-[24px] h-[268px] px-[20px] gap-[10px] justify-center items-center'>
-        <Logo logoClassName='!w-[32px] !h-[32px] mb-[10px]' />
-        <div>
-          <p className={`text-[22px] leading-[32px] text-[#fff] text-center ${BOLD_INTER_TIGHT.className}`}>Projected returns in 5 years</p>
-          <p className={`text-[40px] leading-[42px] text-[#fff] text-center ${BOLD_INTER_TIGHT.className}`}>
-            $ {(initialInvestment * Math.pow(1 + Number((netRentalPerMonth * 12 / Number(formatEther(totalPropertyValue)) * 100 + appreciation / Number(formatEther(totalPropertyValue))).toFixed(3)) / 100, 5)).toLocaleString()}
-          </p>
+      <div className="flex flex-col w-full lg:flex-row gap-0 items-center p-0 max-w-[1200px] m-auto border-[2px] border-[#61CD81] rounded-[24px] overflow-hidden">
+        <div className="flex flex-col flex-1 py-[34px] gap-[20px] justify-between px-[30px] h-[238px]">
+          <div className="flex flex-col gap-[10px]">
+            <h2 className="text-[28px] mlg:text-[32px] leading-[24px] text-tw-text-primary font-bold">
+              Calculate returns
+            </h2>
+            <h2 className="text-[14px] leading-[22px] text-[#0A1339]">
+              Calculate returns effortlessly and make informed financial decisions
+              for a brighter future.
+            </h2>
+          </div>
+          <div className='grid grid-cols-1 md:grid-cols-[minmax(200px,max-content),minmax(310px,max-content),minmax(50px,max-content)] gap-[20px] gap-y-[10px]'>
+            <span className={`text-[14px] leading-[22px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>Initial investment</span>
+            <input 
+              type="range" 
+              min="1000" 
+              max="10000" 
+              value={initialInvestment} 
+              onChange={(e) => setInitialInvestment(Number(e.target.value))} 
+              className="w-full max-w-auto md:max-w-[310px] accent-[#61CD81]"
+            />
+            <span className={`text-[18px] leading-[22px] text-[#61CD81] ${BOLD_INTER_TIGHT.className}`}>${initialInvestment.toLocaleString()}</span>
+            {/* <span className={`text-[14px] leading-[22px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>Monthly recurring investment</span>
+            <input 
+              type="range" 
+              min="100" 
+              max="5000" 
+              value={monthlyInvestment} 
+              onChange={(e) => setMonthlyInvestment(Number(e.target.value))} 
+              className="w-full max-w-[310px] accent-[#61CD81]"
+            />
+            <span className={`text-[18px] leading-[22px] text-[#61CD81] ${BOLD_INTER_TIGHT.className}`}>${monthlyInvestment.toLocaleString()}</span> */}
+          </div>
+          <div className="flex justify-between lg:justify-start items-center gap-[20px] text-text-primary">
+            <div className='flex flex-col items-center'>
+              <p className="text-[12px] leading-[22px] text-center">Annual rent return</p>
+              <p className={`text-[18px] leading-[22px] ${BOLD_INTER_TIGHT.className}`}>{(netRentalPerMonth * 12 / Number(formatEther(totalPropertyValue)) * 100).toFixed(3)} %</p>
+            </div>
+            <div className='flex flex-col items-center'>
+              <p className="text-[12px] leading-[22px] text-center">Annual value growth</p>
+              <p className={`text-[18px] leading-[22px] ${BOLD_INTER_TIGHT.className}`}>{(appreciation / Number(formatEther(totalPropertyValue))).toFixed(3)}%</p>
+            </div>
+            <div className='flex flex-col items-center'>
+              <p className="text-[12px] leading-[22px] text-center">Total annual return</p>
+              <p className={`text-[18px] leading-[22px] ${BOLD_INTER_TIGHT.className}`}>{(netRentalPerMonth * 12 / Number(formatEther(totalPropertyValue)) * 100 + appreciation / Number(formatEther(totalPropertyValue))).toFixed(3)}%</p>
+            </div>
+          </div>
         </div>
-        <Button className='!bg-[#fff] w-full mt-[5px] rounded-[100px] py-[10px]' textClassName={`text-[16px] leading-[24px] ${BOLD_INTER_TIGHT.className}`}>Invest Now   →</Button>
-        <p className="text-[14px] leading-[22px] font-medium text-[#FFFFFFCC]">*Estimated annual returns, using yield statistics from all properties on the platform.</p>
+
+        <div className='flex flex-col w-full max-w-auto bg-[#61CD81] lg:max-w-[400px] overflow-hidden py-[24px] h-[268px] px-[20px] gap-[10px] justify-center items-center'>
+          <Logo logoClassName='!w-[32px] !h-[32px] mb-[10px]' />
+          <div>
+            <p className={`text-[22px] leading-[32px] text-[#fff] text-center ${BOLD_INTER_TIGHT.className}`}>Projected returns in 5 years</p>
+            <p className={`text-[40px] leading-[42px] text-[#fff] text-center ${BOLD_INTER_TIGHT.className}`}>
+              $ {(initialInvestment * Math.pow(1 + Number((netRentalPerMonth * 12 / Number(formatEther(totalPropertyValue)) * 100 + appreciation / Number(formatEther(totalPropertyValue))).toFixed(3)) / 100, 5)).toLocaleString()}
+            </p>
+          </div>
+          <Button 
+            className='!bg-[#fff] w-full mt-[5px] rounded-[100px] py-[10px]' 
+            textClassName={`text-[16px] leading-[24px] ${BOLD_INTER_TIGHT.className}`}
+            onClick={() => scrollToElement()}
+          >
+            Invest Now   →
+          </Button>
+          <p className="text-[14px] leading-[22px] font-medium text-[#FFFFFFCC]">*Estimated annual returns, using yield statistics from all properties on the platform.</p>
+        </div>
       </div>
     </div>
   );
