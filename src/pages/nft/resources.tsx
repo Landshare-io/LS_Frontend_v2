a283edd30a5cf37c8c692f72d0258464f631e724
mogw <mogwinfo@gmail.com>
2024-10-22 16:50:55 +0100

    Added nft resource page

---
+++ src/pages/nft/resources.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import type { NextPage } from 'next';
+import Head from "next/head";
+import { useChainId } from "wagmi";
+import { MAJOR_WORK_CHAIN } from "../../config/constants/environments";
+
+const NftResources: NextPage = () => {
+  const chainId = useChainId();
+
+  return (
+    <div>
+      <Head>
+        <title>Landshare - NFT Resources</title>
+      </Head>
+      {chainId === MAJOR_WORK_CHAIN.id ? (
+        <div className="min-h-[600px] flex flex-col justify-center items-center text-center mt-10 text-red-400 text-xl font-medium animate-[sparkling_3s_linear_infinite]">
+          Chain not Supported / Switch to BSC
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default NftResources;
