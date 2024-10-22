8d8b85a5391ff4f0742b05de18df962ac625561d
mogw <mogwinfo@gmail.com>
2024-10-22 18:57:35 +0100

    Added nft game axios

---
+++ src/hooks/nft-game/axios/nft-game-axios.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { NFT_GAME_BACKEND_URL } from "../../../config/constants/environments"
+
+const ApiService = axios.create({
+  baseURL: NFT_GAME_BACKEND_URL,
+});
+
+// Add a request interceptor to set the Authorization header with a JWT token.
+ApiService.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('jwtToken-v2');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+// API Methods
+const Api = {
+  get(endpoint: string, params?: string) {
+    return ApiService.get(endpoint, { params });
+  },
+
+  post(endpoint: string, data: any) {
+    return ApiService.post(endpoint, data);
+  },
+
+  put(endpoint: string, data: any) {
+    return ApiService.put(endpoint, data);
+  },
+
+  patch(endpoint: string, data: any) {
+    return ApiService.patch(endpoint, data);
+  },
+
+  delete(endpoint: string) {
+    return ApiService.delete(endpoint);
+  },
+};
+
+export default Api;
