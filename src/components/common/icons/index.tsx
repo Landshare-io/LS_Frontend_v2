3d7b1cddc2d124311cc70343f63b6048d1a92536
mogw <mogwinfo@gmail.com>
2024-10-23 14:24:07 +0100

    Added info icon

--- src/components/common/icons/index.tsx
+++ src/components/common/icons/index.tsx
@@ -1,3 +1,5 @@
+import { useGlobalContext } from "../../../context/GlobalContext";
+
 export const BackIcon = ({ className, fillColor }: { className: string, fillColor: string }) => (
   <svg
     className={className}
@@ -45,3 +47,23 @@ export const MonthlyExpenseIcon = ({ className = '', fillColor }: { className?: 
     />
   </svg>
 );
+
+export const InfoIcon = () => {
+  const { theme } = useGlobalContext();
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="cursor-pointer"
+    >
+      <path
+        d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM11 15H9V9H11V15ZM11 7H9V5H11V7Z"
+        fill={theme == 'dark' ? "#cbcbcb" : "#000"}
+        fillOpacity="0.5"
+      />
+    </svg>
+  )
+};
