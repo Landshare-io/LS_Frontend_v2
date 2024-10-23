38ba8a1fc5bceaccd2505391ee79ae39791ef281
mogw <mogwinfo@gmail.com>
2024-10-23 18:23:02 +0100

    Updated useGetItemsByOwner hook

--- src/hooks/contract/PremiumNftContract/useGetItemsByOwner.ts
+++ src/hooks/contract/PremiumNftContract/useGetItemsByOwner.ts
@@ -1,11 +1,10 @@
 import { useReadContract } from "wagmi";
 import { Address } from "viem";
 import PremiumNftAbi from '../../../abis/PremiumNft.json';
-import { MARBLE_COUNTEROPS_CONTRACT_ADDRESS } from "../../../config/constants/environments";
 
-export default function useGetItemsByOwner(chainId: number, address: Address | undefined) {
+export default function useGetItemsByOwner(chainId: number, contractAddress: Address, address: Address | undefined) {
   const { data, isError, isLoading, error } = useReadContract({
-    address: MARBLE_COUNTEROPS_CONTRACT_ADDRESS[chainId],
+    address: contractAddress,
     abi: PremiumNftAbi,
     functionName: "getItemsByOwner",
     chainId: chainId,
@@ -14,7 +13,7 @@ export default function useGetItemsByOwner(chainId: number, address: Address | u
 
   if (isLoading) return []
   if (isError) {
-    console.log('Fetching PoolTable getItemsByOwner error', error)
+    console.log('Fetching PorcelainTile getItemsByOwner error', error)
     return []
   }
 
