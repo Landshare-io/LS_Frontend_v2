44fa48ef823f62a9e67ece2ec4f3702bcb7fc327
mogw <mogwinfo@gmail.com>
2024-10-23 12:07:09 +0100

    Updated you own component

--- src/components/common/you-own/index.tsx
+++ src/components/common/you-own/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { useAccount } from "wagmi";
+import React, { useState } from "react";
+import { useAccount, useChainId } from "wagmi";
 import numeral from "numeral";
-import { formatEther } from "ethers";
+import { BigNumberish, formatEther } from "ethers";
 import { useRouter } from "next/router";
 import {
   LogoIcon,
@@ -12,35 +12,25 @@ import {
   Steel,
   Credit
 } from "../icons/nft";
+import SwitchTheme from "../switch-theme";
 import { useGlobalContext } from "../../../context/GlobalContext";
+import useGetResource from "../../../hooks/nft-game/axios/useGetResource";
+import useGetNftCredits from "../../../hooks/nft-game/apollo/useGetNftCredits";
+import useBalanceOf from "../../../hooks/contract/LandTokenContract/useBalanceOf";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md"
 import LandshareLogo_Dark from "../../../../public/logo-dark.svg"
-import SwitchTheme from "../switch-theme";
 import { BOLD_INTER_TIGHT } from "../../../config/constants/environments";
 
 export default function YouOwn(){
   const { theme } = useGlobalContext();
   const [show, setShow] = useState(false)
-  const { isConnected } = useAccount()
-  const {
-    userResource,
-    updateUserResources,
-    nftCredits
-  } = useGlobalContext();
-  if (!userResource.resource) return null;
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!isConnected) return
-
-    const intervalUpdateUserResource = setInterval(() => updateUserResources(), 60000) // One minute
-
-    return () => clearInterval(intervalUpdateUserResource)
-  }, [isConnected])
-  const styleObject = {
-    minWidth: "180px"
-  };
-
+  const router = useRouter()
+  const { address } = useAccount()
+  const chainId = useChainId()
+  const { data: landTokenBalance } = useBalanceOf({ chainId, address }) as { data: BigNumberish }
+  const { resource, maxPowerLimit } = useGetResource()
+  const { nftCredits } = useGetNftCredits()
+  
   return (
     <section className="bottom-[73px] md:bottom-0 bg-[#bce4fa] duration-500 sticky z-[999] opacity-90 shadow-lg py-3 dark:bg-third">
       <div className="flex justify-center">
@@ -48,8 +38,7 @@ export default function YouOwn(){
           <div className="flex flex-col md:flex-row items-center">
             <div className="flex justify-between w-full">
               <span
-                style={styleObject}
-                className={`mb-2 inline-block font-semibold ${show ? "text-left" : "text-center"} text-text-secondary pr-10 md:pr-0`}
+                className={`min-w-[180px] mb-2 inline-block font-semibold ${show ? "text-left" : "text-center"} text-text-secondary pr-10 md:pr-0`}
               >
                 You own:
               </span>
@@ -71,15 +60,8 @@ export default function YouOwn(){
                     style={{ minWidth: "133px" }}
                     className={`inline-block pl-2 pr-3 text-[16px] text-text-primary ${BOLD_INTER_TIGHT.className}`}
                   >
-                    {userResource.landTokenBalance > 999999999999
-                      ? numeral(
-                        formatEther(userResource.landTokenBalance.toString())
-                      )
-                        .format("0.[00]")
-                        .toString()
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                      : "0"}{" "}
-                    <span className="text-[14px] text-text-primary">LAND</span>
+                    {numeral(formatEther(landTokenBalance)).format("0.[00]").toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    <span className="text-[14px] text-text-primary"> LAND</span>
                   </span>
                   <span
                     className="cursor-pointer hidden hover:shadow-md sm:block text-[#bce4fa] font-normal w-[20px] h-[20px] text-center rounded-full text-[18px] bg-[#000000ba] duration-300 dark:bg-primary dark:text-text-primary"
@@ -91,14 +73,14 @@ export default function YouOwn(){
                 <div className="flex items-center">
                   <Lumber />
                   <span className={`inline-block pl-2 pr-3 text-[16px] text-text-primary ${BOLD_INTER_TIGHT.className}}`}>
-                    {numeral(userResource.resource[1]).format("0.[00]")}
+                    {numeral(resource[1]).format("0.[00]")}
                     <span className="text-[14px] text-text-primary"> Lumber</span>
                   </span>
                 </div>
                 <div className="flex items-center">
                   <Concrete />
                   <span className={`inline-block pl-2 pr-3 text-[16px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>
-                    {numeral(userResource.resource[3]).format("0.[00]")}
+                    {numeral(resource[3]).format("0.[00]")}
                     <span className="text-[14px] text-text-primary"> Concrete</span>
                   </span>
                 </div>
@@ -107,7 +89,7 @@ export default function YouOwn(){
                     <Steel />
                   </span>
                   <span className={`inline-block pl-2 pr-3 text-[16px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>
-                    {numeral(userResource.resource[4]).format("0.[00]")}
+                    {numeral(resource[4]).format("0.[00]")}
                     <span className="fs-xs text-text-primary"> Steel</span>
                   </span>
                 </div>
@@ -119,7 +101,7 @@ export default function YouOwn(){
                     style={{ minWidth: "133px" }}
                     className={`inline-block pl-2 pr-3 text-[16px] text-text-primary ${BOLD_INTER_TIGHT.className}`}
                   >
-                    {numeral(userResource.resource[0]).format("0.[00]")} / {userResource.maxPowerLimit}
+                    {numeral(resource[0]).format("0.[00]")} / {maxPowerLimit}
                     <span className="fs-xs text-text-primary"> Power</span>
                   </span>
                   <span
@@ -134,7 +116,7 @@ export default function YouOwn(){
                     <Brick />
                   </span>
                   <span className={`inline-block pl-2 pr-3 text-[16px] text-text-primary ${BOLD_INTER_TIGHT.className}`}>
-                    {numeral(userResource.resource[2]).format("0.[00]")}
+                    {numeral(resource[2]).format("0.[00]")}
                     <span className="fs-xs text-text-primary"> Brick</span>
                   </span>
                 </div>
