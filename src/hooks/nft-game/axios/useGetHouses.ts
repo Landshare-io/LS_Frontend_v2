bd6e61c5600ff2cfd456e139d0a882d2be8e80a7
mogw <mogwinfo@gmail.com>
2024-10-24 11:55:16 +0100

    Updated useGetHouses hook

--- src/hooks/nft-game/axios/useGetHouses.ts
+++ src/hooks/nft-game/axios/useGetHouses.ts
@@ -1,8 +1,16 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import axios from "./nft-game-axios"
+import { useGlobalContext } from "../../../context/GlobalContext"
 
 export default function useGetHouses() {
+  const { isAuthenticated } = useGlobalContext()
   const [houses, setHouses] = useState([])
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      getHouses()
+    }
+  }, [isAuthenticated])
 
   const getHouses = async () => {
     const { data: houseData } = await axios.get('/house/find-by-user');
