643788c53a779713eb58c7ebb951e2b005299025
mogw <mogwinfo@gmail.com>
2024-10-25 14:18:28 +0100

    Updated mint premium nft card component

--- src/components/mint-premium-nfts/premium-nft.tsx
+++ src/components/mint-premium-nfts/premium-nft.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ReactLoading from "react-loading";
 import ReactModal from 'react-modal';
 import { OpenModalICon } from "../common/icons/index";
+import Button from '../common/button';
 import { useGlobalContext } from '../../context/GlobalContext';
 
 interface PremiumNftProps {
@@ -9,7 +10,7 @@ interface PremiumNftProps {
   premiumNft: any;
   mintCap: number;
   onSubmit: Function;
-  loader: boolean;
+  loader: string;
 }
 
 export default function PremiumNft({ amountMinted, premiumNft, mintCap, onSubmit, loader }: PremiumNftProps) {
@@ -36,22 +37,22 @@ export default function PremiumNft({ amountMinted, premiumNft, mintCap, onSubmit
 
   return (
     <>
-      <div className="d-flex flex-column addon cards-hover-animation">
-        <div className='addon-header'>
-          <div className="d-flex flex-column align-items-center justify-content-center addon-head">
-            <span className="addon-label">{premiumNft.name}</span>
+      <div className="flex flex-col animate-[fadeIn] duration-1200 rounded-[15px] overflow-hidden w-[257px]">
+        <div className='bg-gradient-to-b from-[#68819D] to-[#4da3a942]'>
+          <div className="flex flex-col items-center justif-center bg-[#00000033] h-[49px] py-[8px]">
+            <span className="font-semibold text-white text-[20px]">{premiumNft.name}</span>
           </div>
-          <div className='addon-status position-relative'>
+          <div className='w-full h-[210px] relative'>
             <img
               src={premiumNft.imgSrc}
               alt={premiumNft.name}
-              className="addon-img position-absolute"
+              className="translate-x-[-50%] w-[210px] h-[210px] left-[50%] top-0 absolute"
             />
-            <div className="d-flex position-absolute align-items-end w-100 justify-content-between px-3 addon-current-yield">
-              <div className="current-minted-status">
+            <div className="flex absolute items-end w-full justify-between px-3 bottom-[5px]">
+              <div className="text-[#fff] text-[14px] font-semibold align-text-bottom leading-[18px]">
                 Minted: {amountMinted ?? 0} / Limit {mintCap ?? 0}
               </div>
-              <div className="addon-svg">
+              <div>
                 <div onClick={() => setOpenModal(true)}>
                   <OpenModalICon />
                 </div>
@@ -59,45 +60,45 @@ export default function PremiumNft({ amountMinted, premiumNft, mintCap, onSubmit
             </div>
           </div>
         </div>
-        <div className="yield-upgrade-cost">
-          <div className='d-flex flex-column w-100 buy-or-upgrade-section bg-tw-primary  position-relative green'>
-            <div className='d-flex flex-column px-2'>
-              <div className='d-flex justify-content-between'>
-                <div className='status-value'>
-                  <span className='status-label text-tw-text-secondary'>Multiplier: </span>
+        <div className="bg-[#6f8e9d66] py-[13px] px-[12px]">
+          <div className='flex flex-col w-full h-[130px] rounded-[25px] bg-[#fff] bg-primary relative border-[2px] border-[#61cd81]'>
+            <div className='flex flex-col px-2'>
+              <div className='flex justify-between'>
+                <div>
+                  <span className='text-[#6f8e9d] font-semibold text-[10px] ml-[4px] pr-[2px] text-text-secondary'>Multiplier: </span>
                   <span className={`${theme == 'dark' ? "text-[#cec9c9]" : "text-[#323131]"} text-[12px] font-[600] mr-[4px]`}>+{premiumNft.multiplier}</span>
                   <span className={`${theme == 'dark' ? "text-[#cec9c9]" : "text-[#323131]"} text-[9px] font-[600]`}> /Item</span>
                 </div>
-                <div className='status-value'>
-                  <span className='status-label text-tw-text-secondary'>Refurbish Cost: </span>
+                <div>
+                  <span className='text-[#6f8e9d] font-semibold text-[10px] ml-[4px] pr-[2px] text-text-secondary'>Refurbish Cost: </span>
                   <span className={`${theme == 'dark' ? "text-[#cec9c9]" : "text-[#323131]"} text-[12px] font-[600] mr-[4px]`}>5</span>
                   <span className={`${theme == 'dark' ? "text-[#cec9c9]" : "text-[#323131]"} text-[9px] font-[600]`}>LAND</span>
                 </div>
               </div>
-              <div className="divider w-100 my-2"></div>
+              <div className="border-b-[1px] border-[#00000050] w-full my-2"></div>
             </div>
-            <div className='d-flex pb-3 align-items-center px-2'>
-              <span className='status-label text-tw-text-secondary'>Cost: </span>
-              <div className='status-value w-100'>
-                <div className='next-cost-icons fw-600 ms-1 yield-cost-value green'>
+            <div className='flex pb-3 items-center px-2'>
+              <span className='text-[#6f8e9d] font-semibold text-[10px] ml-[4px] pr-[2px] text-text-secondary'>Cost: </span>
+              <div className='w-full'>
+                <div className='min-w-[20px] font-semibold ml-1 text-[14px] font-normal text-[#00000080]'>
                   {premiumNft.price} LAND
                 </div>
               </div>
             </div>
-            <button
+            <Button
               onClick={() => onSubmit()}
-              className={`btn nav-btn w-100 buy-or-upgrade-btn position-absolute  ${(loader == premiumNft.name) ? 'd-flex justify-content-center align-items-center' : ''}`}
-              disabled //={(loader == premiumNft.name)}
+              className={`w-full bottom-[-1px] h-[45px] text-[18px] font-semibold absolute  ${(loader == premiumNft.name) ? 'flex justify-center items-center' : ''}`}
+              disabled
             >
               {(loader == premiumNft.name) ? (
                 <>
-                  <ReactLoading type="spin" className="me-2 button-spinner" width="24px" height="24px" />
-                  <span className="upgrade-status">Loading</span>
+                  <ReactLoading type="spin" className="mr-2 mb-[4px]" width="24px" height="24px" />
+                  <span className="font-semibold">Loading</span>
                 </>
               ) : (
-                <span className="upgrade-status">Mint</span>
+                <span className="font-semibold">Mint</span>
               )}
-            </button>
+            </Button>
           </div>
         </div>
       </div>
@@ -107,8 +108,8 @@ export default function PremiumNft({ amountMinted, premiumNft, mintCap, onSubmit
           isOpen={openModal}
           onRequestClose={() => { setOpenModal(!openModal), document.body.classList.remove('modal-open'); }}
         >
-          <div className="d-flex min-h-100 justify-content-center align-items-center">
-            <span className="my-2 mx-3 fs-14 fw-400">{premiumNft.infoText}</span>
+          <div className="flex min-h-100 justify-center items-center">
+            <span className="my-2 mx-3 text-[14px]">{premiumNft.infoText}</span>
           </div>
         </ReactModal>
       )}
