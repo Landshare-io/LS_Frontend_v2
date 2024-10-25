a72487d9c9261652f711ed325d01557e8576293f
mogw <mogwinfo@gmail.com>
2024-10-25 15:59:53 +0100

    Working on nft mint page

--- src/components/nft-mint/index.tsx
+++ src/components/nft-mint/index.tsx
@@ -1,21 +1,21 @@
 import { useState, useEffect } from 'react'
 import ReactLoading from "react-loading";
 import { useAccount } from "wagmi";
-import { useGlobalContext } from '../../../contexts/GlobalContext'
-import ConnectWallet from "../../../components/ConnectWallet";
-import { PremiumNfts } from "./premiumNfts/PremiumNfts";
-import Topbar from "../topbar/Topbar";
+import { useGlobalContext } from '../../context/GlobalContext';
+import ConnectWallet from '../connect-wallet';
+import useLogin from '../../hooks/nft-game/axios/useLogin';
+import PremiumNfts from '../mint-premium-nfts';
+import Topbar from '../common/topbar';
 import HouseMintItem from './houseMintItem/houseMintItem';
-import YouOwn from "../YouOwn";
+import YouOwn from '../common/you-own';
 import './mint.css';
 
 export default function NftMint() {
-  const { isConnected } = useAccount();
+  const { isConnected, address } = useAccount();
+  const { isLoading: isLoginLoading } = useLogin(address)
   const {
-    isLoginLoading,
-    signer,
     isAuthenticated,
-    isDarkMode
+    theme
   } = useGlobalContext()
 
   const houseTypes = [
@@ -34,14 +34,14 @@ export default function NftMint() {
   ]
 
   return (
-    <div className={`${isDarkMode ? "dark" : ""} bg-primary`}>
+    <div className={`${theme == 'dark' ? "dark" : ""} bg-primary`}>
       {isLoginLoading ? (
         <div className="flex w-full min-h-[60vh] h-full items-center justify-center">
           <ReactLoading type="bars" color="#61cd81" />
         </div>
       ) : (
         <div className="section-container nft-game-container overflow-hidden pt-0 text-text-primary">
-          {(signer == undefined || !isAuthenticated) ? (
+          {(!isConnected || !isAuthenticated) ? (
             <div className="text-center min-h-60vh d-flex flex-column justify-content-center align-items-center">
               <ConnectWallet />
             </div>
@@ -49,7 +49,7 @@ export default function NftMint() {
             <>
               <Topbar isNftList={true} />
               <span className="fw-bold fs-md">Mint House NFTs</span>
-              <div className={`h-0 border-b ${isDarkMode ? "border-b-[#ffffff50]" : "border-b-[#00000050]"} d-block w-100 mb-4 my-3`}></div>
+              <div className={`h-0 border-b ${theme == 'dark' ? "border-b-[#ffffff50]" : "border-b-[#00000050]"} d-block w-100 mb-4 my-3`}></div>
               <div className='mint-list pb-5 mb-5'>
                 {houseTypes.map((product, index) => (
                   <div
@@ -62,7 +62,7 @@ export default function NftMint() {
               </div>
 
               <span className="fw-bold fs-md">Premium Upgrades</span>
-              <div className={`h-0 border-b ${isDarkMode ? "border-b-[#ffffff50]" : "border-b-[#00000050]"} d-block w-100 mb-4 my-3`}></div>
+              <div className={`h-0 border-b ${theme == 'dark' ? "border-b-[#ffffff50]" : "border-b-[#00000050]"} d-block w-100 mb-4 my-3`}></div>
               <PremiumNfts />
             </>
           )}
