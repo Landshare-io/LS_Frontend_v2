d89023e250b99bc0bd5ac3fe5ce23e4f808acea3
LOLKTKAT <lolktkat@gmail.com>
2024-10-27 00:17:51 +0300

    rwa: style rwa to match old repo

--- src/components/investment-explain/index.tsx
+++ src/components/investment-explain/index.tsx
@@ -10,21 +10,23 @@ export default function InvestmentExplain() {
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth"
+      behavior: "smooth",
     });
   };
 
   return (
     <div className="flex flex-col gap-[40px] items-center p-0 mlg:px-[20px] md:pt-[40px] xl:px-0 xl:pb-[80px] max-w-[1200px] m-auto">
-      <div className="flex flex-col gap-[10px] mb-[20px] items-center">
+      <div className="flex flex-col gap-[10px] items-start lg:items-center">
         <h2
           id="target-element"
-          className={`text-[46px] text-text-primary text-center leading-[59px] font-bold`}
+          className={` text-text-primary lg:text-center text-[32px] md:text-[40px] lg:text-[56px] font-bold`}
         >
           Effortless Property Investment in 3 Steps
         </h2>
-        <p className="text-[14px] leading-[17px] text-text-primary max-w-[719px] text-center">
-          Unlock the world of property investment with ease through Landshare and RWA tokens. Our simple, 3-step process lets you invest in real estate without the complexities of traditional methods.
+        <p className="text-text-secondary text-sm lg:text-center  max-w-[719px] ">
+          Unlock the world of property investment with ease through Landshare
+          and RWA tokens. Our simple, 3-step process lets you invest in real
+          estate without the complexities of traditional methods.
         </p>
       </div>
       <div className="flex flex-col items-center mlg:flex-row w-full gap-[10px]">
@@ -32,11 +34,23 @@ export default function InvestmentExplain() {
         <div className="flex flex-col shrink-0 grow-0 w-full md:w-[270px] h-[354px] items-center justify-between p-[20px] bg-gradient-to-b from-[#61CD81] to-[#2B9F49] rounded-[24px] overflow-hidden relative">
           <div className="flex justify-center gap-[7px]">
             <div className="flex justify-center items-center border border-[#fff] w-[21px] h-[21px] rounded-full">
-              <span className={`text-[14px] leading-[10px] rounded-full text-[#fff] ${BOLD_INTER_TIGHT.className}`}>1</span>
+              <span
+                className={`text-[14px] leading-[10px] rounded-full text-[#fff] ${BOLD_INTER_TIGHT.className}`}
+              >
+                1
+              </span>
             </div>
-            <span className={`text-[20px] leading-[22px] text-[#fff] ${BOLD_INTER_TIGHT.className}`}>Sign up</span>
+            <span
+              className={`text-[20px] leading-[22px] text-[#fff] ${BOLD_INTER_TIGHT.className}`}
+            >
+              Sign up
+            </span>
           </div>
-          <Image src={form} alt="form" className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]" />
+          <Image
+            src={form}
+            alt="form"
+            className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]"
+          />
           <a
             className="!text-[#000] w-full"
             href="https://dashboard.landshare.io/"
@@ -48,18 +62,34 @@ export default function InvestmentExplain() {
         </div>
 
         <div className="flex items-center justify-center w-full h-[100px] mlg:h-full">
-          <Image src={arrowLeft} alt="arrow-left" className="rotate-90 w-fit mlg:rotate-0 mlg:w-full h-[10px] text-[#2A8442]" />
+          <Image
+            src={arrowLeft}
+            alt="arrow-left"
+            className="rotate-90 w-fit mlg:rotate-0 mlg:w-full h-[10px] text-[#2A8442]"
+          />
         </div>
-        
+
         {/* Step 2 */}
         <div className="flex flex-col shrink-0 grow-0 w-full md:w-[270px] h-[354px] items-center justify-between p-[20px] bg-gradient-to-b from-[#61CD81] to-[#2B9F49] rounded-[24px] overflow-hidden relative">
           <div className="flex justify-center gap-[7px]">
             <div className="flex justify-center items-center border border-[#fff] w-[21px] h-[21px] rounded-full">
-              <span className={`text-[14px] leading-[10px] rounded-full text-[#fff] ${BOLD_INTER_TIGHT.className}`}>2</span>
+              <span
+                className={`text-[14px] leading-[10px] rounded-full text-[#fff] ${BOLD_INTER_TIGHT.className}`}
+              >
+                2
+              </span>
             </div>
-            <span className={`text-[20px] leading-[22px] text-[#fff] ${BOLD_INTER_TIGHT.className}`}>Purchase Tokens</span>
+            <span
+              className={`text-[20px] leading-[22px] text-[#fff] ${BOLD_INTER_TIGHT.className}`}
+            >
+              Purchase Tokens
+            </span>
           </div>
-          <Image src={swapToken} alt="swapToken" className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]" />
+          <Image
+            src={swapToken}
+            alt="swapToken"
+            className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]"
+          />
           <button
             onClick={() => scrollToTop()}
             className="!bg-[#fff] hover:bg-slate-200 transition-all hover:-translate-y-1 w-full rounded-[100px] py-[10px] text-[16px]  leading-[24px] font-bold"
@@ -69,18 +99,34 @@ export default function InvestmentExplain() {
         </div>
 
         <div className="flex items-center justify-center w-full h-[100px] mlg:h-full">
-          <Image src={arrowLeft} alt="arrow-left" className="rotate-90 w-fit mlg:rotate-0 mlg:w-full h-[10px] text-[#2A8442]" />
+          <Image
+            src={arrowLeft}
+            alt="arrow-left"
+            className="rotate-90 w-fit mlg:rotate-0 mlg:w-full h-[10px] text-[#2A8442]"
+          />
         </div>
-        
+
         {/* Step 3 */}
         <div className="flex flex-col shrink-0 grow-0 w-full md:w-[270px] h-[354px] items-center justify-between p-[20px] bg-gradient-to-b from-[#61CD81] to-[#2B9F49] rounded-[24px] overflow-hidden relative">
           <div className="flex justify-center gap-[7px]">
             <div className="flex justify-center items-center border border-[#fff] w-[21px] h-[21px] rounded-full">
-              <span className={`text-[14px] leading-[10px] rounded-full text-[#fff] ${BOLD_INTER_TIGHT.className}`}>3</span>
+              <span
+                className={`text-[14px] leading-[10px] rounded-full text-[#fff] ${BOLD_INTER_TIGHT.className}`}
+              >
+                3
+              </span>
             </div>
-            <span className={`text-[20px] leading-[22px] text-[#fff] ${BOLD_INTER_TIGHT.className}`}>Hold & Earn</span>
+            <span
+              className={`text-[20px] leading-[22px] text-[#fff] ${BOLD_INTER_TIGHT.className}`}
+            >
+              Hold & Earn
+            </span>
           </div>
-          <Image src={growthTime} alt="growthTime" className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]" />
+          <Image
+            src={growthTime}
+            alt="growthTime"
+            className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]"
+          />
           <a
             className="w-full !text-[#000]"
             href=" https://docs.landshare.io/platform-features/landshare-rwa-token-lsrwa/rwa-token-utilities"
@@ -90,7 +136,7 @@ export default function InvestmentExplain() {
             </button>
           </a>
         </div>
-        
+
         {/* Step 4 */}
         {/* <div className="flex flex-col w-full md:max-w-[270px] h-[354px] items-center justify-between p-[20px] bg-gradient-to-b from-[#61CD81] to-[#2B9F49] rounded-[24px] overflow-hidden relative">
           <div className="flex justify-center gap-[7px]">
@@ -104,5 +150,5 @@ export default function InvestmentExplain() {
         </div> */}
       </div>
     </div>
-  )
+  );
 }
