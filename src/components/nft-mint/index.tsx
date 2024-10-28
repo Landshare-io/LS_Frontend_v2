5cccfd131e6fa03e3521db0a5c8e7f20c13a1ab2
mogw <mogwinfo@gmail.com>
2024-10-28 13:36:10 +0000

    Fixed nft mint page

--- src/components/nft-mint/index.tsx
+++ src/components/nft-mint/index.tsx
@@ -7,7 +7,7 @@ import PremiumNfts from '../mint-premium-nfts';
 import Topbar from '../common/topbar';
 import HouseMintItem from './house-mint';
 import YouOwn from '../common/you-own';
-import './mint.css';
+import { BOLD_INTER_TIGHT } from "../../config/constants/environments";
 
 export default function NftMint() {
   const { isConnected, address } = useAccount();
@@ -39,29 +39,29 @@ export default function NftMint() {
           <ReactLoading type="bars" color="#61cd81" />
         </div>
       ) : (
-        <div className="section-container nft-game-container overflow-hidden pt-0 text-text-primary">
+        <div className="relative max-w-[1200px] px-0 m-auto overflow-hidden pt-0 text-text-primary">
           {(!isConnected || !isAuthenticated) ? (
-            <div className="text-center min-h-60vh d-flex flex-column justify-content-center align-items-center">
+            <div className="text-center min-h-[60vh] flex flex-col justify-center items-center">
               <ConnectWallet />
             </div>
           ) : (
             <>
               <Topbar isNftList={true} />
-              <span className="fw-bold fs-md">Mint House NFTs</span>
-              <div className={`h-0 border-b ${theme == 'dark' ? "border-b-[#ffffff50]" : "border-b-[#00000050]"} d-block w-100 mb-4 my-3`}></div>
-              <div className='mint-list pb-5 mb-5'>
+              <span className={`text-[14px] ${BOLD_INTER_TIGHT.className}`}>Mint House NFTs</span>
+              <div className={`h-0 border-b ${theme == 'dark' ? "border-b-[#ffffff50]" : "border-b-[#00000050]"} block w-full mb-4 my-3`}></div>
+              <div className='flex overflow-x-auto pb-[20px] lg:grid lg:gird-cols-[minmax(251px,max-content),minmax(251px,max-content),minmax(251px,max-content),minmax(251px,max-content)] justify-between gap-[4rem] pb-5 mb-5'>
                 {houseTypes.map((product, index) => (
                   <div
                     key={`nft-house-item-${index}`}
-                    className="mint-item-moible"
+                    className="max-w-[251px] mr-[20px] m-0"
                   >
                     <HouseMintItem product={product} />
                   </div>
                 ))}
               </div>
 
-              <span className="fw-bold fs-md">Premium Upgrades</span>
-              <div className={`h-0 border-b ${theme == 'dark' ? "border-b-[#ffffff50]" : "border-b-[#00000050]"} d-block w-100 mb-4 my-3`}></div>
+              <span className={`text-[14px] ${BOLD_INTER_TIGHT.className}`}>Premium Upgrades</span>
+              <div className={`h-0 border-b ${theme == 'dark' ? "border-b-[#ffffff50]" : "border-b-[#00000050]"} block w-full mb-4 my-3`}></div>
               <PremiumNfts />
             </>
           )}
