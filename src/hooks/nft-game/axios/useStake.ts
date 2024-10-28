64245f0eb0281ecf29194c7e7e0d632c94031e85
mogw <mogwinfo@gmail.com>
2024-10-28 13:40:05 +0000

    Added useStake hook

---
+++ src/hooks/nft-game/axios/useStake.ts
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+import { BigNumberish } from "ethers";
+import { Address } from "viem";
+import { useWaitForTransactionReceipt } from "wagmi";
+import axios from "./nft-game-axios";
+import { useGlobalContext } from "../../../context/GlobalContext";
+import useApprove from "../../contract/RWAContract/useApprove";
+import useDeposit from "../../contract/AssetStakeContract/useDeposit";
+import useStakedBalance from "../../contract/AssetStakeContract/useStakedBalance";
+import { ASSET_STAKE_CONTRACT_ADDRESS } from "../../../config/constants/environments";
+import useGetUserData from "./useGetUserData";
+
+export default function useStake(chainId: number, address: Address | undefined, setDepositLoading: Function) {
+  const [depositAmount, setDepositAmount] = useState<BigNumberish>(0)
+  const { approve, data: approveTx } = useApprove(chainId)
+  const { deposit, data: depositTx } = useDeposit(chainId)
+  const { notifyError, notifySuccess } = useGlobalContext()
+  const { refetch: refetchStakedAmount } = useStakedBalance(chainId, address)
+  const { getUserData } = useGetUserData()
+
+  const { isSuccess: approveSuccess } = useWaitForTransactionReceipt({
+    hash: approveTx,
+    chainId: chainId
+  });
+  const { isSuccess: depositSuccess } = useWaitForTransactionReceipt({
+    hash: depositTx,
+    chainId: chainId
+  });
+
+  useEffect(() => {
+    (async () => {
+      if (approveTx) {
+        if (approveSuccess) {
+          try {
+            deposit(depositAmount)
+          } catch (error: any) {
+            setDepositLoading(false)
+            notifyError(error.response.data.message);
+          }
+        }
+      }
+    })()
+  }, [approveTx, approveSuccess])
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.post('/house/deposit-asset-token', {
+          houseId: -1
+        })
+
+        if (data.status) {
+          setDepositAmount(0)
+          refetchStakedAmount()
+          getUserData()
+          setDepositLoading(false);
+          notifySuccess(`${depositAmount} Asset Tokens deposited!`);
+        } else {
+          setDepositLoading(false);
+          notifyError("Deposit failed");
+        }
+      } catch (error: any) {
+      setDepositLoading(false)
+      notifyError(error.response.data.message);
+    }
+    })()
+  }, [depositTx, depositSuccess])
+  
+
+  const stake = async (amount: BigNumberish) => {
+    try {
+      setDepositAmount(amount)
+      approve(ASSET_STAKE_CONTRACT_ADDRESS[chainId], amount)
+    } catch (error: any) {
+      setDepositLoading(false)
+      notifyError(error.response.data.message);
+    }
+  }
+
+  return { stake }
+}
