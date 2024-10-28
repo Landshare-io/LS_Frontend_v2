a467721a93bbd65a405febc97bcfbc234e661b13
mogw <mogwinfo@gmail.com>
2024-10-28 11:31:40 +0000

    Added nft inventory page

---
+++ src/pages/nft/inventory.tsx
@@ -0,0 +1,19 @@
+import { useChainId } from 'wagmi'
+import InventoryPage from './inventory'
+import { supportChainIds } from '../../wagmi'
+
+export default function Inventory() {
+  const chainId = useChainId() as 56 | 137 | 42161 | 97 | 11155111 | 80002
+
+  return (
+    <div>
+      {!supportChainIds.includes(chainId) ? (
+        <div className="min-h-[600px] flex flex-col justify-center items-center text-center mt-10 text-red-400 text-xl font-medium animate-[sparkling-anim_3s_linear_infinite]">
+          Chain not Supported / Switch to BSC
+        </div>
+      ) : (
+        <InventoryPage />
+      )}
+    </div>
+  )
+}
