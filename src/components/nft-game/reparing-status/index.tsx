78b91c3af5b13c5750fb1dbf811342d9b76abaec
mogw <mogwinfo@gmail.com>
2024-10-29 14:40:36 +0000

    Added reparing status component

---
+++ src/components/nft-game/reparing-status/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import numeral from 'numeral';
+import ProgressBar from '../../common/progressbar';
+import './ReparingStatus.css'
+
+interface ReparingStatusProps {
+  max: number
+  now: number
+}
+
+export default function ReparingStatus({ max, now }: ReparingStatusProps) {
+  const showNow = Number(numeral(Number(now)).format('0.[0]'))
+
+  return (
+    <div className='progress-content-show' style={{ width: '100%' }}>
+      {max > 100 ? (
+        showNow > 100 ? (
+          <ProgressBar min={0} max={max} now={showNow} label={`${showNow}%`} animated={true} />
+        ) : (
+          <div className='d-flex w-100'>
+            <div style={{ width: ((100 / max) * 100) + '%' }} className='empty-status'>
+              <ProgressBar min={0} max={100} now={showNow} label={`${showNow}%`} animated={true} />
+            </div>
+            <div style={{ width: ((100 * (max - 100)) / max) + '%' }} className='empty-status-progress'>
+            </div>
+          </div>
+        )
+      ) : (
+        <ProgressBar min={0} max={max} now={showNow} label={`${showNow}%`} animated={true} />
+      )}
+    </div>
+  );
+};
