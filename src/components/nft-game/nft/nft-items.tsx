e7a1ff4855962d782a8868845c0b308ecd19abf4
mogw <mogwinfo@gmail.com>
2024-10-29 12:40:46 +0000

    Added nft items component

---
+++ src/components/nft-game/nft/nft-items.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
+import Image from "next/image";
+import { NextLabelIcon, PrevLabelIcon, QuestionIcon } from "../../common/icons/index";
+import NftItem from "./NftItem";
+import sadEmoji from "../../../../public/icons/sad_emoji.png";
+import sadEmoji_Dark from "../../../../public/icons/no_item_dark.png";
+import { useGlobalContext } from "../../../context/GlobalContext";
+import { BOLD_INTER_TIGHT } from "../../../config/constants/environments";
+
+interface NftItemsProps {
+  houseItems: any[];
+}
+
+
+export default function NftItems({ houseItems }: NftItemsProps) {
+  const [itemOffset, setItemOffset] = useState(0);
+  const { theme } = useGlobalContext();
+  const handlePageClick = (event: any) => {
+    const newOffset = (event.selected * itemsPerPage) % houseItems.length;
+    setItemOffset(newOffset);
+  };
+
+  const itemsPerPage = 8;
+  const pageCount = Math.ceil(houseItems.length / itemsPerPage);
+  const currentItems = houseItems.slice(itemOffset, itemOffset + itemsPerPage);
+
+  return (
+    <>
+      <div className="">
+        {houseItems.length > 0 ? (
+          <div className="flex overflow-x-scroll pb-[20px] lg:grid lg:grid-cols-[minmax(251px,max-content),minmax(251px,max-content),minmax(251px,max-content)] lg:justify-between lg:gap-[4rem] xl:grid-cols-[minmax(251px,max-content),minmax(251px,max-content),minmax(251px,max-content),minmax(251px,max-content)] mb-5">
+            {currentItems.map((houseItem, index) => {
+              return (
+                <div
+                  className="flex justify-center mb-[20px] mlg:mb-[40px]"
+                  key={`nft-house-item-${itemOffset + index}`}
+                >
+                  <NftItem
+                    house={
+                      houseItems[itemOffset + index]
+                    }
+                  />
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="flex w-full h-full items-center justify-center flex-col min-h-[40vh]">
+            <div className={`text-[20px] mb-[20px] text-gray-500 ${BOLD_INTER_TIGHT.className}`}>No NFTs Found</div>
+            <Image src={theme == 'dark' ? sadEmoji_Dark : sadEmoji} alt="Sad Emoji" />
+            <div className="flex justify-center items-center mt-[20px]">
+              <a
+                href="https://docs.landshare.io/"
+                target="_blank"
+                className="text-center text-[16px] mr-[5px] dark:text-text-secondary"
+              >
+                Learn More
+              </a>{" "}
+              <QuestionIcon />
+            </div>
+          </div>
+        )}
+        {pageCount > 1 && (
+          <div className="flex justify-center mb-5">
+            <ReactPaginate
+              previousLabel={<PrevLabelIcon />}
+              nextLabel={<NextLabelIcon />}
+              containerClassName={"pagination"}
+              activeClassName={"active"}
+              breakLabel="..."
+              onPageChange={handlePageClick}
+              marginPagesDisplayed={1}
+              pageRangeDisplayed={
+                itemOffset === 0
+                  ? 5
+                  : itemOffset / itemsPerPage + 2 <= 4
+                    ? 4
+                    : pageCount - itemOffset / itemsPerPage <= 3
+                      ? 5
+                      : 2
+              }
+              forcePage={itemOffset / itemsPerPage}
+              pageCount={pageCount}
+              renderOnZeroPageCount={null}
+            />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
