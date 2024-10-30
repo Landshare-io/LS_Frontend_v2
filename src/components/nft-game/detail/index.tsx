d4829a35a9e5bc74f219aa9af626df8120aa0c93
mogw <mogwinfo@gmail.com>
2024-10-30 13:05:28 +0000

    Working on nft game detail component

---
+++ src/components/nft-game/detail/index.tsx
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useAccount } from "wagmi";
+import ReactLoading from "react-loading";
+
+import NftDetails from "./NftDetails";
+import Topbar from "../../common/topbar";
+import YouOwn from "../../common/you-own";
+import MainTabs from "../../components/common/tab/Tab";
+import { PremiumNfts } from "./premiumNfts/PremiumNfts";
+import { UpgradeListTable } from "./upgradeListTable/UpgradeListTable";
+import { ProductionUpgrade } from "./productionUpgrade/ProductionUpgrade";
+import { UpgradeSection } from "./upgradeSection/UpgradeSection";
+
+import { useGlobalContext } from "../../../context/GlobalContext";
+import ConnectWallet from "../../components/ConnectWallet";
+
+import useGetHouse from "../../../hooks/nft-game/axios/useGetHouse";
+
+const NftPage = () => {
+  const { isConnected } = useAccount();
+  const {
+    isAuthenticated,
+  } = useGlobalContext();
+
+  const router = useRouter()
+  const { houseId } = router.query as { houseId: string };
+  const { house, setHouse, getHouse, isLoading: isPageLoading } = useGetHouse(houseId);
+
+
+  const tabItems = [
+    {
+      id: "Yield Upgrades",
+      children: (
+        <UpgradeSection
+          house={house}
+          setHouse={setHouse}
+        />
+      ),
+    },
+    {
+      id: "Production Upgrades",
+      children: (
+        <ProductionUpgrade
+          house={house}
+          setHouse={setHouse}
+        />
+      ),
+    },
+    {
+      id: "Premium Upgrades",
+      children: (
+        <PremiumNfts
+          house={house}
+          getHouse={getHouse}
+        />
+      ),
+    },
+    {
+      id: "Upgrade List Table",
+      children: (
+        <UpgradeListTable
+          house={house}
+        />
+      ),
+    },
+  ];
+
+
+  useEffect(() => {
+    if (!isAuthenticated) return;
+    if (!house.isActivated) return;
+
+    // const interval = setInterval(() => {
+    getHouse();
+    // }, 60000);
+
+    // return () => clearInterval(interval);
+  }, [isAuthenticated]);
+
+  return (
+    <>
+      {isPageLoading ? (
+        <div className="flex w-full min-h-[60vh] h-full items-center justify-center">
+          <ReactLoading type="bars" color="#61cd81" />
+        </div>
+      ) : (
+        <>
+          <div className="relative max-w-[1200px] px-0 m-auto flex flex-col pt-0 bg-tw-primary">
+            {(!isAuthenticated) ? (
+              <div className="text-center min-h-[60vh] flex flex-col justify-center items-center">
+                <ConnectWallet />
+              </div>
+            ) : (
+              <>
+                <Topbar isNftList={false} />
+                <NftDetails
+                  house={house}
+                  setHouse={setHouse}
+                  getHouse={getHouse}
+                />
+                {house.name && (
+                  <div className="mainTabs-styles">
+                    <MainTabs tabItems={tabItems} />
+                  </div>
+                )}
+              </>
+            )}
+          </div>
+          {isConnected && isAuthenticated && (
+            <YouOwn />
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default NftPage;
