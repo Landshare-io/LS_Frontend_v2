59a343b1eb835bc1792f4f2377a7a591b28c91fc
mogw <mogwinfo@gmail.com>
2024-10-30 14:52:20 +0000

    Added repair cost list component

---
+++ src/components/nft-game/detail/repairing-cost-list.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  BrickIcon,
+  ChargeIcon,
+  ConcreteIcon,
+  LumberIcon,
+  SteelIcon,
+} from "../../common/icons/nft";
+import { useGlobalContext } from "../../../context/GlobalContext";
+import { BOLD_INTER_TIGHT } from "../../../config/constants/environments";
+
+export default function ReparingCostList({ cost }: { cost: any }) {
+  const { theme } = useGlobalContext();
+  const icons = {
+    'power': <ChargeIcon className="" iconColor={theme == 'dark' ? "#cbcbcb" : "#000000"} />,
+    'lumber': <LumberIcon className="" iconColor={theme == 'dark' ? "#cbcbcb" : "#000000"} />,
+    'brick': <BrickIcon className="" iconColor={theme == 'dark' ? "#cbcbcb" : "#000000"} />,
+    'concrete': <ConcreteIcon className="" iconColor={theme == 'dark' ? "#cbcbcb" : "#000000"} />,
+    'steel': <SteelIcon className="" iconColor={theme == 'dark' ? "#cbcbcb" : "#000000"} />,
+  } as Record<string, any>;
+  return (
+    <>
+      {Object.keys(cost).map((key: string) => (
+        <div
+          key={key}
+          className="flex grow p-1 items-center text-[14px] text-tw-text-secondary"
+        >
+          <span className={`pr-[2px] ${BOLD_INTER_TIGHT.className}`}>{Math.ceil(cost[key] * 10) / 10}</span>
+          {icons[key]}
+        </div>
+      ))}
+    </>
+  );
+};
