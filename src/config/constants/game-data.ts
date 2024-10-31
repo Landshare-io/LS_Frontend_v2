ede01b6fdb2a4ff73cdb79f7d72e9c804a309bb7
mogw <mogwinfo@gmail.com>
2024-10-31 17:21:53 +0000

    Working on game data constants

---
+++ src/config/constants/game-data.ts
@@ -0,0 +1,139 @@
+import Garden from "../../assets/img/yield-upgrades/garden.png";
+import Landscaping from "../../assets/img/yield-upgrades/landscaping.png";
+import KitchenRemodel from "../../assets/img/yield-upgrades/kitchen_remodel.png";
+import BathroomRemodel from "../../assets/img/yield-upgrades/bathroom_remodel.png";
+import SteelSiding from "../../assets/img/yield-upgrades/steel_siding.png";
+import SteelAppliances from "../../assets/img/yield-upgrades/steel_appliances.png";
+import HardwoodFloors from "../../assets/img/yield-upgrades/hardwood_floors.png";
+import JacuzziTub from "../../assets/img/yield-upgrades/jacuzzi_tub.png";
+import FinishedBasement from "../../assets/img/yield-upgrades/finished_basement.png";
+import Cellar from "../../assets/img/yield-upgrades/cellar.png";
+import Trees from "../../assets/img/yield-upgrades/trees.png";
+import Fireplace from '../../assets/img/yield-upgrades/fireplace.png'
+
+import Toolshed1 from "../../assets/img/production-upgrade/toolshed1.png";
+import Toolshed2 from "../../assets/img/production-upgrade/toolshed2.png";
+import Toolshed3 from "../../assets/img/production-upgrade/toolshed3.png";
+import Toolshed4 from "../../assets/img/production-upgrade/toolshed4.png";
+import FatificationBrick from "../../assets/img/production-upgrade/fatification_brick.png";
+import FatificationConcrete from "../../assets/img/production-upgrade/fatification_concrete.png";
+import FatificationSteel from "../../assets/img/production-upgrade/fatification_steel.png";
+import Harvester from "../../assets/img/production-upgrade/harvester.png";
+import ConcreteFoundation from "../../assets/img/production-upgrade/concrete_foundation.png";
+import Firepit from "../../assets/img/production-upgrade/firepit.png";
+import Handyman from "../../assets/img/production-upgrade/handyman.png";
+import Generator from "../../assets/img/production-upgrade/generator.png";
+
+export const yieldUpdgradesData = [
+  {
+    title: "Hardwood Floors",
+    imgUrl: HardwoodFloors
+  },
+  {
+    title: "Landscaping",
+    imgUrl: Landscaping
+  },
+  {
+    title: "Garden",
+    imgUrl: Garden
+  },
+  {
+    title: "Trees",
+    imgUrl: Trees
+  },
+  {
+    title: "Fireplace",
+    imgUrl: Fireplace
+  },
+  {
+    title: "Kitchen Remodel",
+    imgUrl: KitchenRemodel
+  },
+  {
+    title: "Bathroom Remodel",
+    imgUrl: BathroomRemodel
+  },
+  {
+    title: "Jacuzzi Tub",
+    imgUrl: JacuzziTub
+  },
+  {
+    title: "Steel Sliding",
+    imgUrl: SteelSiding
+  },
+  {
+    title: "Steel Application",
+    imgUrl: SteelAppliances
+  },
+  {
+    title: "Cellar",
+    imgUrl: Cellar
+  },
+  {
+    title: "Finished Basement",
+    imgUrl: FinishedBasement
+  }
+];
+
+export const productionUpdgradesData = [
+  {
+    title: "Lumber Toolshed",
+    imgUrl: Toolshed1,
+    sortingId: 0
+  },
+  {
+    title: "Brick Toolshed",
+    imgUrl: Toolshed2,
+    sortingId: 0
+  },
+  {
+    title: "Concrete Toolshed",
+    imgUrl: Toolshed3,
+    sortingId: 0
+  },
+  {
+    title: "Steel Toolshed",
+    imgUrl: Toolshed4,
+    sortingId: 0
+  },
+  {
+    title: "Firepit",
+    imgUrl: Firepit,
+    sortingId: 1
+  },
+  {
+    title: "Concrete Foundation",
+    imgUrl: ConcreteFoundation,
+    sortingId: 2
+  },
+  {
+    title: "Harvester",
+    imgUrl: Harvester,
+    sortingId: 3
+  },
+  {
+    title: "Fortification Brick",
+    imgUrl: FatificationBrick,
+    sortingId: 4
+  },
+  {
+    title: "Fortification Concrete",
+    imgUrl: FatificationConcrete,
+    sortingId: 4
+  },
+  {
+    title: "Fortification Steel",
+    imgUrl: FatificationSteel,
+    sortingId: 4
+  },
+  {
+    title: "Hire Handman",
+    imgUrl: Handyman,
+    sortingId: 5
+  },
+  {
+    title: "Generator",
+    imgUrl: Generator,
+    sortingId: 6
+  }
+]
