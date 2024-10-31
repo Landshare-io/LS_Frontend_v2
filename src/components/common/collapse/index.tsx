ff1a621d3e6062115f584b56193d6ecc0f2199e3
mogw <mogwinfo@gmail.com>
2024-10-31 14:23:56 +0000

    Fixed code style

--- src/components/common/collapse/index.tsx
+++ src/components/common/collapse/index.tsx
@@ -10,18 +10,18 @@ export default function Collapse({ children, isOpen }: CollapseProps) {
   return (
     <div className="w-full">
       <AnimatePresence>
-      {isOpen && (
-        <motion.div 
-        key={+isOpen}
-        initial={{height: 0}}
-        animate={{height: "fit-content"}}
-        exit={{height: 0}}
-        transition={{duration: 0.25, ease: "easeInOut"}}
-        
-        className={`transition-[max-height] ease-in-out duration-300 overflow-hidden`}>
-          {children}
-        </motion.div>
-      )}
+        {isOpen && (
+          <motion.div 
+            key={+isOpen}
+            initial={{height: 0}}
+            animate={{height: "fit-content"}}
+            exit={{height: 0}}
+            transition={{duration: 0.25, ease: "easeInOut"}}  
+            className={`transition-[max-height] ease-in-out duration-300 overflow-hidden`}
+          >
+            {children}
+          </motion.div>
+        )}
       </AnimatePresence>
     </div>
   );
