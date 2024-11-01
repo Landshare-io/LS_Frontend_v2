db15a5c85f3e3915ff7f338389d214727d9d13c5
mogw <mogwinfo@gmail.com>
2024-11-01 10:43:01 +0000

    Updated PremiumNftContract's useIsApproveForAll hook

--- src/hooks/contract/PremiumNftContract/useIsApproveForAll.ts
+++ src/hooks/contract/PremiumNftContract/useIsApproveForAll.ts
@@ -1,28 +1,22 @@
-import { useWriteContract } from 'wagmi'
+import { useReadContract } from 'wagmi'
 import { Address } from 'viem';
 import PremiumNft from "../../../abis/PremiumNft.json";
 import { ADMIN_WALLET_ADDRESS } from "../../../config/constants/environments";
 
-export default function useIsApprovedForAll() {
-  const {
-    data,
-    isPending,
-    writeContract
-  } = useWriteContract();
+export default function useIsApprovedForAll(chainId: number, contractAddress: Address, address: Address | undefined) {
+  const { data, isError, isLoading, error, refetch } = useReadContract({
+    address: contractAddress,
+    abi: PremiumNft,
+    functionName: "isApprovedForAll",
+    args: [address, ADMIN_WALLET_ADDRESS],
+    chainId: chainId
+  })
 
-  async function isApprovedForAll(chainId: number, contractAddress: Address, address: Address | undefined) {
-    await writeContract({
-      address: contractAddress,
-      abi: PremiumNft,
-      functionName: "isApprovedForAll",
-      chainId: chainId,
-      args: [address, ADMIN_WALLET_ADDRESS]
-    })
+  if (isLoading) return { data: false, refetch, isLoading }
+  if (isError) {
+    console.log('Fetching premiumNft isApprovedForAll error', error)
+    return { data: false, refetch, isLoading: false }
   }
 
-  return {
-    isApprovedForAll,
-    isPending,
-    data
-  }
+  return { data, refetch, isLoading }
 }
