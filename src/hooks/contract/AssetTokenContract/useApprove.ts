49c36b1749cda2949dd355bf69a6d7d36c425cca
mogw <mogwinfo@gmail.com>
2024-11-08 17:06:44 +0000

    Added useApprove hooks of asset tokens

---
+++ src/hooks/contract/AssetTokenContract/useApprove.ts
@@ -0,0 +1,30 @@
+import { useWriteContract } from 'wagmi'
+import { Address } from 'viem';
+import { BigNumberish } from 'ethers';
+import { bsc } from 'viem/chains';
+import AssetToken from "../../../abis/AssetToken.json";
+import { ASSET_TOKEN_ADDRESS } from "../../../config/constants/environments";
+
+export default function useApprove() {
+  const {
+    data,
+    isPending,
+    writeContract
+  } = useWriteContract();
+
+  async function approve(approveAddress: Address, amount: number | BigNumberish) {
+    await writeContract({
+      address: ASSET_TOKEN_ADDRESS,
+      abi: AssetToken,
+      functionName: "approve",
+      chainId: bsc.id,
+      args: [approveAddress, amount]
+    });
+  }
+
+  return {
+    approve,
+    isPending,
+    data
+  }
+}
