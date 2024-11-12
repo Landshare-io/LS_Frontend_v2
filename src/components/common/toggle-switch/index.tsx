04dd70445ed3beeb89999cb258776c4239a263a5
mogw <mogwinfo@gmail.com>
2024-11-12 17:10:31 +0000

    Added toggle switch component

---
+++ src/components/common/toggle-switch/index.tsx
@@ -0,0 +1,30 @@
+import { useGlobalContext } from "../../../context/GlobalContext";
+
+interface ToggleSwitchProps {
+  isSale: boolean
+  className?: string
+  onClick: Function
+  disabled?: boolean
+}
+
+
+export default function ToggleSwitch({ isSale, className, onClick, disabled }: ToggleSwitchProps) {
+  const { theme } = useGlobalContext()
+
+  return (
+    <button
+      className={`toggle-switch flex gap-[2px] ${isSale ? 'sale' : 'off-sale'} ${isSale ? "bg-[#fff]" : "bg-[#808080]"} ml-4 ${className}`}
+      type="button"
+      aria-pressed="false"
+      onClick={() => onClick()}
+      disabled={disabled}
+    >
+      <span className="text-white text-[11px]">
+        ON
+      </span>
+      <span className="text-white text-[11px]">
+        OFF
+      </span>
+    </button>
+  )
+}
