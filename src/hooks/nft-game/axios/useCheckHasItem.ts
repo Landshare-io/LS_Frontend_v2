fb362a28cb67309bfe630f1d40e15f230751a3f0
mogw <mogwinfo@gmail.com>
2024-11-12 14:56:16 +0000

    Fixed useCheckHasItem hook

--- src/hooks/nft-game/axios/useCheckHasItem.ts
+++ src/hooks/nft-game/axios/useCheckHasItem.ts
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "./nft-game-axios";
 
-export default function useCheckHasItem(item: any, houseId: number) {
+export default function useCheckHasItem(item: any, houseId: number | string) {
   const [hasItem, setHasItem] = useState<any>({})
 
   useEffect(() => {
+    if (typeof houseId === "undefined" || houseId === '') return
     if (typeof item !== 'undefined' && typeof item.id !== 'undefined') {
       const fetchItem = async () => {
         const { data } = await axios.get(`/has-item/find-item-and-house-and-user?itemId=${item.id}&houseId=${houseId}`);
@@ -12,7 +13,7 @@ export default function useCheckHasItem(item: any, houseId: number) {
       };
       fetchItem();
     }
-  }, [item]);
+  }, [item, houseId]);
 
   return hasItem
 }
