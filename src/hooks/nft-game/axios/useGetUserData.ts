f4cd65de38fdf7e70fcb390ec374d0d3ac6e7100
mogw <mogwinfo@gmail.com>
2024-11-21 15:31:20 +0000

    Updated useGetUserData hook

--- src/hooks/nft-game/axios/useGetUserData.ts
+++ src/hooks/nft-game/axios/useGetUserData.ts
@@ -2,22 +2,57 @@ import { useState, useEffect } from "react";
 import axios from "./nft-game-axios";
 import { useGlobalContext } from "../../../context/GlobalContext";
 
+let userDataState = {
+  id: null,
+  activatedSlots: 2,
+  depositedBalance: 0,
+}
+let facilitiesState: any[] = []
+
+// Subscribers to update all components on state change
+const subscribers = new Set<Function>();
+
+// Helper to update all subscribers
+const notifySubscribers = () => {
+  subscribers.forEach((callback) => callback());
+};
+
 export default function useGetUserData() {
-  const [userData, setUserData] = useState({
-    id: null,
-    activatedSlots: 2,
-    depositedBalance: 0,
-  });
-  const [facilities, setFacilities] = useState<any[]>([])
+  const [userData, setUserData] = useState(userDataState);
+  const [facilities, setFacilities] = useState<any[]>(facilitiesState)
   const { isAuthenticated } = useGlobalContext()
+
+  useEffect(() => {
+    // Subscribe on mount
+    const update = () => {
+      setUserData(userDataState)
+      setFacilities(facilitiesState)
+    };
+    subscribers.add(update);
+
+    // Cleanup on unmount
+    return () => {
+      subscribers.delete(update);
+    };
+  }, []);
+
+  const updateUserData = (newUserData: any) => {
+    userDataState = newUserData;
+    notifySubscribers();
+  };
+
+  const updateFacilities = (newFacilities: any[]) => {
+    facilitiesState = newFacilities;
+    notifySubscribers();
+  };
 
   const getUserData = async () => {
     try {
       const { data: userInfo } = await axios.get(`/user/get-detail`)
       const { data: facilityData } = await axios.get(`/user/facility`)
 
-      setUserData(userInfo)
-      setFacilities(facilityData.sort((a: any, b: any) => a.sortingId - b.sortingId))
+      updateUserData(userInfo)
+      updateFacilities(facilityData.sort((a: any, b: any) => a.sortingId - b.sortingId))
     } catch (error: any) {
       console.log(error.response.data.message, error)
     }
@@ -33,7 +68,7 @@ export default function useGetUserData() {
     userData,
     facilities,
     getUserData,
-    setFacilities,
-    setUserData
+    setFacilities: updateFacilities,
+    setUserData: updateUserData
   }
 }
