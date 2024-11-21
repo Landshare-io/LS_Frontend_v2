07df2a8a442d0bf21f63a487feed710ee9cc01be
mogw <mogwinfo@gmail.com>
2024-11-21 15:30:09 +0000

    Updated useGetGameItems hook

--- src/hooks/nft-game/axios/useGetGameItems.ts
+++ src/hooks/nft-game/axios/useGetGameItems.ts
@@ -2,12 +2,61 @@ import { useState, useEffect } from "react";
 import axios from "./nft-game-axios";
 import { useGlobalContext } from "../../../context/GlobalContext";
 
+let yieldUpgradesListState: any[] = []
+let productionUpgradesListState: any[] = []
+let premiumUpgradesListState: any[] = []
+let boostItemsListState: any[] = []
+
+// Subscribers to update all components on state change
+const subscribers = new Set<Function>();
+
+// Helper to update all subscribers
+const notifySubscribers = () => {
+  subscribers.forEach((callback) => callback());
+};
+
 export default function useGetGameItems() {
   const { isAuthenticated } = useGlobalContext();
-  const [yieldUpgradesList, setYieldUpgradesList] = useState<any[]>([]);
-  const [productionUpgradesList, setProductionUpgradesList] = useState<any[]>([]);
-  const [premiumUpgradesList, setPremiumUpgradesList] = useState<any[]>([]);
-  const [boostItemsList, setBoostItemsList] = useState<any[]>([]);
+  const [yieldUpgradesList, setYieldUpgradesList] = useState<any[]>(yieldUpgradesListState);
+  const [productionUpgradesList, setProductionUpgradesList] = useState<any[]>(productionUpgradesListState);
+  const [premiumUpgradesList, setPremiumUpgradesList] = useState<any[]>(premiumUpgradesListState);
+  const [boostItemsList, setBoostItemsList] = useState<any[]>(boostItemsListState);
+
+  useEffect(() => {
+    // Subscribe on mount
+    const update = () => {
+      setYieldUpgradesList(yieldUpgradesListState);
+      setProductionUpgradesList(productionUpgradesListState)
+      setPremiumUpgradesList(premiumUpgradesListState)
+      setBoostItemsList(boostItemsListState)
+    };
+    subscribers.add(update);
+
+    // Cleanup on unmount
+    return () => {
+      subscribers.delete(update);
+    };
+  }, []);
+
+  const updateYieldUpgrades = (newYieldUpgrades: any[]) => {
+    yieldUpgradesListState = newYieldUpgrades;
+    notifySubscribers();
+  };
+
+  const updateProductionUpgrades = (newProductionUpgrades: any[]) => {
+    productionUpgradesListState = newProductionUpgrades;
+    notifySubscribers();
+  };
+
+  const updatePremiumUpgrades = (newPremiumUpgrades: any[]) => {
+    premiumUpgradesListState = newPremiumUpgrades;
+    notifySubscribers();
+  };
+
+  const updateBoostItems = (newBoostItems: any[]) => {
+    boostItemsListState = newBoostItems;
+    notifySubscribers();
+  };
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -25,19 +74,19 @@ export default function useGetGameItems() {
 
         const { data: fetchedItems } = await axios.get(`/item/${itemType.id}`);
         if (itemType.name == "Yield Upgrades") {
-          setYieldUpgradesList(fetchedItems)
+          updateYieldUpgrades(fetchedItems)
         }
 
         if (itemType.name == "Production Upgrades") {
-          setProductionUpgradesList(fetchedItems)
+          updateProductionUpgrades(fetchedItems)
         }
 
         if (itemType.name == "Premium Upgrades") {
-          setPremiumUpgradesList(fetchedItems)
+          updatePremiumUpgrades(fetchedItems)
         }
 
         if (itemType.name == "Boost Items") {
-          setBoostItemsList(fetchedItems)
+          updateBoostItems(fetchedItems)
         }
       }
     } catch (error: any) {
