d615ef5c8beb38684fd9a1c0db0a5fdf740fe507
mogw <mogwinfo@gmail.com>
2024-11-21 15:29:59 +0000

    Updated useCheckHasLandscaping hook

--- src/hooks/nft-game/axios/useCheckHasLandscaping.ts
+++ src/hooks/nft-game/axios/useCheckHasLandscaping.ts
@@ -1,8 +1,36 @@
 import { useEffect, useState } from "react"
 import axios from "./nft-game-axios"
 
+let hasLandscapingState = false
+
+// Subscribers to update all components on state change
+const subscribers = new Set<Function>();
+
+// Helper to update all subscribers
+const notifySubscribers = () => {
+  subscribers.forEach((callback) => callback());
+};
+
 export default function useCheckHasLandscaping(houseId: number | string) {
-  const [hasLandscaping, setHasLandscaping] = useState(false)
+  const [hasLandscaping, setHasLandscaping] = useState(hasLandscapingState)
+
+  useEffect(() => {
+    // Subscribe on mount
+    const update = () => {
+      setHasLandscaping(hasLandscapingState);
+    };
+    subscribers.add(update);
+
+    // Cleanup on unmount
+    return () => {
+      subscribers.delete(update);
+    };
+  }, []);
+
+  const updateHasLandscaping = (newHasLandscaping: boolean) => {
+    hasLandscapingState = newHasLandscaping;
+    notifySubscribers();
+  };
   
   useEffect(() => {
     (async () => {
@@ -15,12 +43,12 @@ export default function useCheckHasLandscaping(houseId: number | string) {
       if (typeof houseId === "undefined" || houseId === '') return
       const { data } = await axios.get(`/has-item/has-landscaping/${houseId}`)
 
-      setHasLandscaping(data)
+      updateHasLandscaping(data)
     } catch (error: any) {
       console.log(error.response.data.message, error)
-      setHasLandscaping(false)
+      updateHasLandscaping(false)
     }
   }
 
-  return { hasLandscaping, setHasLandscaping, checkHasLandscaping }
+  return { hasLandscaping, setHasLandscaping: updateHasLandscaping, checkHasLandscaping }
 }
