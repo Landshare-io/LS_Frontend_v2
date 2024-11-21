e0e356b56ea628449261e38f1133a8dbbc6a20c6
mogw <mogwinfo@gmail.com>
2024-11-21 12:51:27 +0000

    Updated useCheckHasGarden hook

--- src/hooks/nft-game/axios/useCheckHasGarden.ts
+++ src/hooks/nft-game/axios/useCheckHasGarden.ts
@@ -1,8 +1,37 @@
 import { useEffect, useState } from "react"
 import axios from "./nft-game-axios";
 
+let hasGardenState = false
+
+// Subscribers to update all components on state change
+const subscribers = new Set<Function>();
+
+// Helper to update all subscribers
+const notifySubscribers = () => {
+  subscribers.forEach((callback) => callback());
+};
+
 export default function useCheckHasGarden(houseId: number | string) {
-  const [hasGarden, setHasGarden] = useState(false)
+  const [hasGarden, setHasGarden] = useState(hasGardenState)
+
+  useEffect(() => {
+    // Subscribe on mount
+    const update = () => {
+      setHasGarden(hasGardenState);
+    };
+    subscribers.add(update);
+
+    // Cleanup on unmount
+    return () => {
+      subscribers.delete(update);
+    };
+  }, []);
+
+  const updateHasGarden = (newHasGarden: boolean) => {
+    hasGardenState = newHasGarden;
+    notifySubscribers();
+  };
+  
 
   useEffect(() => {
     (async () => {
@@ -15,12 +44,12 @@ export default function useCheckHasGarden(houseId: number | string) {
       if (typeof houseId === "undefined" || houseId === '') return
       const { data } = await axios.get(`/has-item/has-garden/${houseId}`)
 
-      setHasGarden(data)
+      updateHasGarden(data)
     } catch (error: any) {
       console.log(error.response.data.message, error)
-      setHasGarden(false)
+      updateHasGarden(false)
     }
   }
 
-  return { hasGarden, setHasGarden, checkHasGarden }
+  return { hasGarden, setHasGarden: updateHasGarden, checkHasGarden }
 }
