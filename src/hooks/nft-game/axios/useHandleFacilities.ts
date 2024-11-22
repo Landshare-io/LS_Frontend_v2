c9234a0109f9f8bfcc3efb2eaaac854ced59303b
mogw <mogwinfo@gmail.com>
2024-11-22 14:53:28 +0000

    Updated useHandleFacilities hook

--- src/hooks/nft-game/axios/useHandleFacilities.ts
+++ src/hooks/nft-game/axios/useHandleFacilities.ts
@@ -6,7 +6,7 @@ import useLogin from "./useLogin";
 import { validateResource } from "../../../utils/helpers/validator";
 import { useGlobalContext } from "../../../context/GlobalContext";
 
-export default function useHandleFacilities(address: Address | undefined) {
+export default function useHandleFacilities(address: Address | undefined, setIsLoading: Function) {
   const { checkIsAuthenticated } = useLogin()
   const { facilities, setFacilities } = useGetUserData()
   const { resource, setResource, maxPowerLimit, setMaxPowerLimit } = useGetResource()
@@ -16,6 +16,7 @@ export default function useHandleFacilities(address: Address | undefined) {
     if (!facilities[type].nextFacility) return notifyError(`Your ${facilities[type].currentFacility.name} level is max.`)
 
     if (await validateResource(resource, facilities[type].nextFacility?.buy.slice(2, 7))) {
+      setIsLoading({ type: type, loading: true });
       try {
         const { data } = await axios.post('/has-item/upgrade-facility', {
           nextFacilityId: facilities[type].nextFacility.id,
@@ -35,14 +36,17 @@ export default function useHandleFacilities(address: Address | undefined) {
 
         setResource([data.resource.power, data.resource.lumber, data.resource.brick, data.resource.concrete, data.resource.steel])
         setMaxPowerLimit(type == 0 ? data.currentFacility.rewardLimit[2] : maxPowerLimit)
+        setIsLoading({ type: -1, loading: false });
         return notifySuccess(`Upgraded ${data.currentFacility.name} successfully!`)
       } catch (error: any) {
+        setIsLoading({ type: -1, loading: false });
         if (error.response?.data.status == 401) {
           checkIsAuthenticated(address)
         } else
           return notifyError(error.response.data.message);
       }
     } else {
+      setIsLoading({ type: -1, loading: false });
       return notifyError('Insufficient resources.')
     }
   }
