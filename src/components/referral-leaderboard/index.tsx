0dc97213a7d62d00bebb88ffc7efd26ef9a76cac
happysuperevil <happysuperevil601@gmail.com>
2024-11-22 10:48:14 -0500

    feat : full leaderboard api integration

--- src/components/referral-leaderboard/index.tsx
+++ src/components/referral-leaderboard/index.tsx
@@ -9,65 +9,93 @@ import {
   TableCell,
 } from "../common/table";
 import { Fuul } from '@fuul/sdk';
+import { useAccount } from "wagmi";
+import { getCurrentEpoch } from "../../utils/helpers/generate-epochs";
 
 interface leaderboardDataProps {
   rank: number;
   account: string;
   total_amount: number;
-  total_attributions: number;
-  // totalTakerVolume: string;
-  // tier: string;
+  referred_users: number;
+  referred_volume: number;
 }
 
 export default function ReferralLeaderBoard() {
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
   const [pageCount, setPageCount] = useState<number>(0);
   const [leaderboardData, setLeaderboardData] = useState<leaderboardDataProps[]>();
+  const [myLeaderboard, setMyLeaderboard] = useState<leaderboardDataProps[]>();
+  const [myRank, setMyRank] = useState<number>(0);
+  const {address} = useAccount();
+  const current_epoch = getCurrentEpoch();
 
   useEffect(()=>{
     const fetchData = async () => {
-      const res = await Fuul.getPointsLeaderboard({});
-
-      setPageCount(res.results.length / 10 + 1);
+      const res = await Fuul.getPointsLeaderboard({
+        page : currentPage,
+        page_size : pageSize, 
+        user_type : 'affiliate',
+        fields: 'referred_volume,referred_users',
+        from: current_epoch?.start_date ? new Date(current_epoch.start_date) : undefined,
+        to: current_epoch?.end_date ? new Date(current_epoch.end_date) : undefined,
+      });
 
       const formattedData = res?.results?.map((item: any) => ({
         rank: item.rank,
-        account: item.account ?? 0, // Provide a default value if account is missing
+        account: item.account,
         total_amount: item.total_amount,
-        total_attributions: item.total_attributions,
+        referred_users: item.referred_users,
+        referred_volume : item.referred_volume
       }));
 
+      setPageCount(res.total_results); 
       setLeaderboardData(formattedData);
     }
 
     fetchData();
-  }, [])
+  }, [currentPage]);
 
-  const getCurrentEpoch = () => {
-    const today = new Date();
-    
-    const currentMonthIndex = today.getMonth();
-    
-    const previousMonthIndex = (currentMonthIndex + 11) % 12;
-    const nextMonthIndex = (currentMonthIndex + 1) % 12;     
+  useEffect(()=>{
+    const fetchData = async () => {
+      const res = await Fuul.getPointsLeaderboard({
+        user_address : address,
+        page : currentPage,
+        page_size : pageSize, 
+        user_type : 'affiliate',
+        fields: 'referred_volume,referred_users',
+        from: current_epoch?.start_date ? new Date(current_epoch.start_date) : undefined,
+        to: current_epoch?.end_date ? new Date(current_epoch.end_date) : undefined,
+      });
 
-    const monthNames = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
+      const formattedData = res?.results?.map((item: any) => ({
+        rank: item.rank,
+        account: item.account,
+        total_amount: item.total_amount,
+        referred_users: item.referred_users,
+        referred_volume : item.referred_volume
+      }));
 
-    const previousMonth = monthNames[previousMonthIndex];
-    const currentMonth = monthNames[currentMonthIndex];
-    const nextMonth = monthNames[nextMonthIndex];
+      setMyLeaderboard(formattedData);
+    }
 
-    return `${previousMonth} 25 - ${nextMonth} 25`;
-}
+    fetchData();
+  }, []);
+
+  const formatEpochDates = (startDate?: string, endDate?: string): string => {
+    const startMonth = startDate ? new Date(startDate).toLocaleString('default', { month: 'long' }) : 'Unknown';
+    const endMonth = endDate ? new Date(new Date(endDate).setMonth(new Date(endDate).getMonth() + 1)).toLocaleString('default', { month: 'long' }) : 'Unknown';
+    return `${startMonth} 25 - ${endMonth} 25`;
+  }
 
   return (
     <div className="mt-12">
       <h2 className="text-text-primary font-bold text-2xl leading-[22px]">
         Leaderboard
-        <span className="px-2 w-fit my-6 text-text-primary text-sm font-normal">{getCurrentEpoch()}  <span className="text-[#61CD81]">ⓘ</span></span>
+        <span className="px-2 w-fit my-6 text-text-primary text-sm font-normal">
+          {formatEpochDates(current_epoch?.start_date, current_epoch?.end_date)}
+        <span className="text-[#61CD81]"> {"ⓘ"}</span>
+        </span>
       </h2>
 
       <div className="mt-[10px] text-text-secondary text-sm">The program operates in 3-month epochs.</div>
@@ -89,61 +117,75 @@ export default function ReferralLeaderBoard() {
 
         <TableBody>
           {leaderboardData?.slice(10 * (currentPage - 1), 10 * (currentPage - 1) + 10).map((data, index) => {
-            const { rank, account, total_amount, total_attributions } =
+            const { rank, account, total_amount, referred_users, referred_volume } =
               data;
             const accountDisplay = `${account.slice(0, 6)}...${account.slice(
               -4
             )}`;
-            return (
+            
+            if(address !==account){
+              return (
+                <TableRow
+                  className="bg-secondary mb-3 shadow-md shadow-gray-400/10 rounded-xl"
+                  key={index}
+                >
+                  <TableCell className="rounded-l-xl">{rank}</TableCell>
+                  <TableCell>{accountDisplay}</TableCell>
+                  <TableCell>{total_amount}</TableCell>
+                  <TableCell>{referred_users}</TableCell>
+                  <TableCell className="rounded-r-xl">{referred_volume}</TableCell>
+                </TableRow>
+              );
+            }else{
               <TableRow
-                className="bg-secondary mb-3 shadow-md shadow-gray-400/10 rounded-xl"
-                key={index}
-              >
-                <TableCell className="rounded-l-xl">{rank}</TableCell>
-                <TableCell>{accountDisplay}</TableCell>
-                <TableCell>{total_amount}</TableCell>
-                <TableCell>{"0"}</TableCell>
-                <TableCell className="rounded-r-xl">{"totalTakerVolume"}</TableCell>
+                  className="mb-3 shadow-md shadow-gray-400/10 rounded-xl text-[#0B6C96] bg-[#0B6C961A] font-bold"
+                >
+                <TableCell className="rounded-l-xl border border-r-0 border-[#0B6C9680]">{rank}</TableCell>
+                <TableCell className="border-y border-[#0B6C9680]">{accountDisplay}</TableCell>
+                <TableCell className="border-y border-[#0B6C9680]">{total_amount}</TableCell>
+                <TableCell className="border-y border-[#0B6C9680]">{referred_users}</TableCell>
+                <TableCell className="rounded-r-xl border border-l-0 border-[#0B6C9680]">{referred_volume}</TableCell>
               </TableRow>
-            );
+            }
           })}
 
-          {/* test Data */}
-          
-          <TableRow
-              className="bg-secondary mb-3 shadow-md shadow-gray-400/10 rounded-xl"
-            >
-            <TableCell className="rounded-l-xl">{"1"}</TableCell>
-            <TableCell >{"0xf362…8247"}</TableCell>
-            <TableCell >{"85,613.67 USDC"}</TableCell>
-            <TableCell >{"0"}</TableCell>
-            <TableCell className="rounded-r-xl">{"totalTakerVolume"}</TableCell>
-          </TableRow>
-
-          <TableRow
-              className="mb-3 shadow-md shadow-gray-400/10 rounded-xl text-[#0B6C96] bg-[#0B6C961A] font-bold"
-            >
-            <TableCell className="rounded-l-xl border border-r-0 border-[#0B6C9680]">{"1"}</TableCell>
-            <TableCell className="border-y border-[#0B6C9680]">{"0xf362…8247"}</TableCell>
-            <TableCell className="border-y border-[#0B6C9680]">{"85,613.67 USDC"}</TableCell>
-            <TableCell className="border-y border-[#0B6C9680]">{"0"}</TableCell>
-            <TableCell className="rounded-r-xl border border-l-0 border-[#0B6C9680]">{"totalTakerVolume"}</TableCell>
-          </TableRow>
-
+          {
+            myLeaderboard?.map((data, index) => {
+              const { rank, account, total_amount, referred_users, referred_volume } =
+                data;
+              const accountDisplay = `${account.slice(0, 6)}...${account.slice(
+                -4
+              )}`;
+              setMyRank(rank);
+            return (
+              <>
+                <TableRow
+                  className="bg-secondary mb-3 shadow-md shadow-gray-400/10 rounded-xl"
+                  key={index}
+                >
+                  <TableCell className="rounded-l-xl">{rank}</TableCell>
+                  <TableCell>{accountDisplay}</TableCell>
+                  <TableCell>{total_amount}</TableCell>
+                  <TableCell>{referred_users}</TableCell>
+                  <TableCell className="rounded-r-xl">{referred_volume}</TableCell>
+                </TableRow>
+              </>
+            );
+          })}
         </TableBody>
       </Table>
 
-      <div className="flex justify-between">
-        <div className="text-text-primary font-bold text-base">
-          Your rank: 34 
-        </div>
+      <div className="w-full flex justify-between items-center">
+            <div className="text-text-primary font-bold text-base">
+              Your rank: {myRank} 
+            </div>
 
-        <Pagination
-          pageCount={pageCount}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
-      </div>
+            <Pagination
+              pageCount={pageCount}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
+        </div>
     </div>
   );
 }
