11b2312056a709a177e468dfc57fb4a5523f1fbf
happysuperevil <happysuperevil601@gmail.com>
2024-11-22 09:49:59 -0500

    feat : integrate individual reward api integration

--- src/components/referral-overview/index.tsx
+++ src/components/referral-overview/index.tsx
@@ -1,20 +1,47 @@
 import { Fuul } from '@fuul/sdk';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import Slider from "../common/slider";
+import { getCurrentEpoch } from '../../utils/helpers/generate-epochs';
 
 export default function ReferralOverview() {
   const {address} = useAccount();
+  const [pendingInvites, setPendingInvites] = useState<number>(0);
+  const [approvedInvites, setApprovedInvites] = useState<number>(0);
+  const [purchaseVolume, setPurchaseVolume] = useState<number>(0);
+  const [referredVolume, setReferredVolume] = useState<number>(0);
 
   useEffect(() => {
     const fetchData = async () => {
+      const current_epoch = getCurrentEpoch();
+      
       if(address){
-        const res = await Fuul.getPointsLeaderboard({ 
+        const pending_users = await Fuul.getPointsLeaderboard({ 
           user_address: address, 
-          from: new Date('2000-01-01'),
-          to: new Date(),
-          user_type: 'end_user',
+          from: current_epoch?.start_date ? new Date(current_epoch.start_date) : undefined,
+          to: current_epoch?.end_date ? new Date(current_epoch.end_date) : undefined,
+          user_type: 'end_user', //If user reach out first conversation, pending users can assume as total
+          fields: 'referred_volume',
+          conversions : '3'
         });
+
+        setPendingInvites(pending_users.results.length);
+
+        const invited_users = await Fuul.getPointsLeaderboard({ 
+          user_address: address, 
+          from: current_epoch?.start_date ? new Date(current_epoch.start_date) : undefined,
+          to: current_epoch?.end_date ? new Date(current_epoch.end_date) : undefined,
+          user_type: 'end_user', //After completed all conversations, these are approved users 
+          fields: 'referred_volume',
+          conversions : '4'
+        });
+
+        const totalPurchaseAmountSum = [...pending_users.results, ...invited_users.results].reduce((sum, item) => sum + Number(item?.total_amount), 0);
+        const totalReferredAmountSum = [...invited_users.results].reduce((sum, item) => sum + Number(item?.referred_volume), 0);
+
+        setPurchaseVolume(totalPurchaseAmountSum);
+        setReferredVolume(totalReferredAmountSum);
+        setApprovedInvites(invited_users.results.length)
       }
     }
     
@@ -33,7 +60,7 @@ export default function ReferralOverview() {
             Pending invites <span className="text-[#61CD81]">ⓘ</span>
           </p>
 
-          <p className="font-bold text-lg text-text-primary leading-7">0</p>
+          <p className="font-bold text-lg text-text-primary leading-7">{pendingInvites - approvedInvites}</p>
         </div>
 
         <div className="pr-6">
@@ -41,7 +68,7 @@ export default function ReferralOverview() {
             Total Approved Invites
           </p>
 
-          <p className="font-bold text-lg text-text-primary leading-7">1</p>
+          <p className="font-bold text-lg text-text-primary leading-7">{approvedInvites}</p>
         </div>
 
         <div>
@@ -50,7 +77,7 @@ export default function ReferralOverview() {
           </p>
           
           <p className="font-bold text-lg text-text-primary leading-7">
-            0 USDT
+            {purchaseVolume} USDT
           </p>
         </div>
       </div>
@@ -58,7 +85,14 @@ export default function ReferralOverview() {
       <div className="w-full">
         <p className="text-text-primary text-sm">4 invites remaining</p>
 
-        <Slider percentage = {10}/>
+        <div className='flex justify-between items-center gap-6'>
+          <Slider percentage = {20}/>
+
+          <div className='w-20 shrink-0'>
+            <p className='text-text-secondary text-base'>Earn Bonus</p>
+            <p className='text-text-primary font-bold text-lg'>{referredVolume} USDC</p>
+          </div>  
+        </div>
 
         <p className="text-text-secondary text-sm">20% completed</p>
       </div>
