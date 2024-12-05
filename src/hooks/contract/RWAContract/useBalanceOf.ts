25dbdd8286c2613decd268c746aa073db15f611e
mogw <mogwinfo@gmail.com>
2024-12-05 19:11:26 +0000

    Updated RWAcontract's useBalanceOf hook

--- src/hooks/contract/RWAContract/useBalanceOf.ts
+++ src/hooks/contract/RWAContract/useBalanceOf.ts
@@ -6,10 +6,10 @@ import { Address } from "viem";
 
 export default function useBalanceOf(chainId: number, address: Address | undefined) {
   const { data, isError, isLoading, error, refetch } = useReadContract({
-    address: RWA_CONTRACT_ADDRESS[bsc.id],
+    address: RWA_CONTRACT_ADDRESS[chainId],
     abi: RwaContractAbi,
     functionName: "balanceOf",
-    chainId: bsc.id,
+    chainId: chainId,
     args: [address]
   })
 
