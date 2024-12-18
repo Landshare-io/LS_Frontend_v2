487e2dbf52a2b561e5dcf068e7b2b302f14e23fa
oxhop <takaursa@gmail.com>
2024-12-19 04:05:56 +0900

    [NFT/Marketplace] Fixed background color

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -15,7 +15,7 @@ const Nft: NextPage = () => {
   }, [address])
 
   return (
-    <div>
+    <div className='bg-primary'>
       {!supportChainIds.includes(chainId) ? (
         <div className="min-h-[600px] flex flex-col justify-center items-center text-center mt-10 text-red-400 text-xl font-medium animate-[sparkling-anim_3s_linear_infinite]">
           Chain not Supported / Switch to BSC
