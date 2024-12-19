0e7355e5705acfc1b7b85c470fa3adfc53b35bd2
oxhop <takaursa@gmail.com>
2024-12-20 01:10:08 +0900

    [Vaults] Fixed location of auto icon of Auto LAND Staking

--- src/components/vaults/auto-vault.tsx
+++ src/components/vaults/auto-vault.tsx
@@ -308,7 +308,7 @@ export default function AutoVault({
                   </button>
                 </div>
                 <div className="flex items-center py-[6px] justify-start h-[100px] gap-[16px]" onClick={() => setDetails(!details)}>
-                  <div className="w-[100px] h-[100px] shrink-0 rounded-[1000px] md:relative">
+                  <div className="w-[100px] h-[100px] shrink-0 rounded-[1000px] relative">
                     <Image src={theme == 'dark' ? UnionDark : Union} className="border-primary border-[6px] rounded-[1000px] w-[90px] h-[90px] absolute left-0 top-0" alt="token pair" />
                     <Image src={theme == 'dark' ? smallicondark : smallicon} className="border-primary border-[6px] rounded-[1000px] w-[40px] h-[40px] absolute right-0 bottom-0 bg-secondary" alt="" />
                   </div>
