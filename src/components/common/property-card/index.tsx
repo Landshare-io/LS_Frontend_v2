fddca8cf0d12ab17b242c295eec4a805d6e0774e
oxhop <takaursa@gmail.com>
2024-12-20 00:05:25 +0900

    [Asset Summary] Fixed border radius of rental property card

--- src/components/common/property-card/index.tsx
+++ src/components/common/property-card/index.tsx
@@ -29,19 +29,19 @@ export default function PropertyCard({ property }: PropertyCardProps) {
 
   return (
     <div className="pb-[10px] md:w-fit md:rounded-[24px] md:px-[20px] md:pb-[24px] max-w-[501px] relative cursor-pointer">
-      <div className="relative rounded-[24px] shadow shadow-lg z-[3] overflow-hidden" onClick={() => router.push(`/tokenized-asset/${property?.id}`)}>
+      <div className="relative rounded-[24px] shadow-lg z-[3] overflow-hidden" onClick={() => router.push(`/tokenized-asset/${property?.id}`)}>
         <SkeletonTheme baseColor={`${theme == 'dark' ? "#31333b" : "#dbdde0"}`} highlightColor={`${theme == 'dark' ? "#52545e" : "#f6f7f9"}`}>
-          <div className="relative rounded-[24px] shadow shadow-lg z-[3] w-full md:w-[390px] md:h-[292px]">
+          <div className="relative rounded-[24px] shadow-lg z-[3] w-full md:w-[390px] md:h-[292px]">
             <img className="w-full h-full object-cover rounded-t-[24px]" src={property?.pictures[0]} alt="property image" />
             <div className="absolute text-[14px] leading-[22px] tracking-[0.02em] text-[#ffffff] flex gap-[7px] left-0 bottom-0 bg-gradient-to-b from-transparent to-[#00000096] py-[4px] px-[8px] w-full">
               <Image src={IconLocation} alt="location" />
               <div className="overflow-hidden text-ellipsis whitespace-nowrap">{property.address}</div>
             </div>
-            <span className={`absolute left-0 top-[20px] shadow-md rounded-r-[15px] text-[12px] leading-[20px] tracking-[0.02em] text-[0.02em] text-[#fff] py-[4px] px-[15px] ${property.type == "Rental Property" ? "bg-[#FF5454]" : "bg-[#3B98EE]"} ${BOLD_INTER_TIGHT.className}`}>
+            <span className={`absolute left-0 top-[20px] shadow-md rounded-r-[15px] leading-[20px] tracking-[0.02em] text-[0.02em] text-[#fff] py-[4px] px-[15px] ${property.type == "Rental Property" ? "bg-[#FF5454]" : "bg-[#3B98EE]"} ${BOLD_INTER_TIGHT.className}`}>
               {property.type}
             </span>
           </div>
-          <div className="flex flex-col py-[20px] px-[30px] gap-[24px] bg-third rounded-b-[24px]">
+          <div className="flex flex-col py-[20px] px-[30px] gap-[24px] bg-third">
             <div className="flex justify-between">
               <div className="flex flex-col items-center">
                 <span className="text-[12px] leading-[22px] text-center tracking-[0.24px] text-text-third">Property Value</span>
@@ -57,18 +57,18 @@ export default function PropertyCard({ property }: PropertyCardProps) {
               </div>
             </div>
             <div className="py-0 px-[10px] sm:p-0 flex justify-between">
-              <span className="text-[12px] leading-[20px] md:text-[14px] md:leading-[22px] rounded-[20px] tracking-[0.02em] md:text-[14px] md:leading-[22px] text-[#61CD81] md:py-[4px] md:px-[16px] bg-primary">Single Family</span>
+              <span className="text-[12px] leading-[20px] rounded-[20px] tracking-[0.02em] md:text-[14px] md:leading-[22px] text-[#61CD81] md:py-[4px] md:px-[16px] bg-primary">Single Family</span>
               <div className="flex gap-[10px] md:gap-[20px]">
                 {property?.bedrooms &&
                   <div className="flex items-center gap-[5px]">
                     <Image src={IconSofa} alt="sofa" />
-                    <div className="text-[12px] leading-[20px] text-[14px] font-medium leading-[22px] text-text-primary">{property?.bedrooms}</div>
+                    <div className="text-[14px] font-medium leading-[22px] text-text-primary">{property?.bedrooms}</div>
                   </div>
                 }
                 {property?.bathrooms &&
                   <div className="flex items-center gap-[5px]">
                     <Image src={IconBathroom} alt="bathroom" />
-                    <div className="text-[12px] leading-[20px] text-[14px] font-medium leading-[22px] text-text-primary">{property?.bathrooms}</div>
+                    <div className="text-[14px] font-medium leading-[22px] text-text-primary">{property?.bathrooms}</div>
                   </div>
                 }
               </div>
