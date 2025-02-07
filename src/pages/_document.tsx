f42f1ea6cb12245a82fc7b9f175b8a00ca04d33d
danila <kolisnichenkodanila@gmail.com>
2025-02-07 19:46:12 +0530

    fix: fix 4 space format issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-	return (
-		<Html>
-			<Head>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
+  return (
+    <Html>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
               try {
                 const theme = localStorage.getItem('land-v2-theme');
                 if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -16,13 +16,13 @@ export default function Document() {
                 console.error('Failed to set initial theme', e);
               }
             `,
-					}}
-				/>
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
