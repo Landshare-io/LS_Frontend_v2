339b494cf6d69694b103552d7bce78973c58f335
Jordan Friske <jordanfriske87@gmail.com>
2025-03-10 15:08:54 -0500

    updated date, need perma solutions

--- src/components/investment-house-detail/house-info-carousel.tsx
+++ src/components/investment-house-detail/house-info-carousel.tsx
@@ -557,7 +557,7 @@ export default function HouseInfoCarouselComponent({
                 <BsShieldFillCheck className="text-primary-green" />
                 Secured & Regulated Investment
               </p>
-              <p className="text-[10px] lg:text-[12px]">Rates updated: Today</p>
+              <p className="text-[10px] lg:text-[12px]">Rates updated: March 5th, 2025</p>
             </div>
           </div>
         </>
