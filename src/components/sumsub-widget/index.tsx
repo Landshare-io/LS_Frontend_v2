ec45042103be4d9093eb2dc70e4a77a870778d40
Jordan Friske <jordanfriske87@gmail.com>
2025-07-20 12:49:11 -0500

    fixed build errors

--- src/components/sumsub-widget/index.tsx
+++ src/components/sumsub-widget/index.tsx
@@ -7,44 +7,51 @@ import { SUMSUB_VERIFY_URL } from '../../config/constants/environments';
 export default function KYCWidget() {
   const { address } = useAccount();
 
-  async function launchWebSdk() {
-    console.log('Launching Sumsub Web SDK for address:', address);
+  async function getNewAccessToken() {
     const { data } = await axios.post(`${SUMSUB_VERIFY_URL}/access-token`, {
       externalUserId: address,
-    })
+    });
+    return data.token;
+  }
 
-    let snsWebSdkInstance = snsWebSdk.init(
-        data.token,
-        () => (this as any).getNewAccessToken()
-      )
+  async function launchWebSdk() {
+    console.log('Launching Sumsub Web SDK for address:', address);
+
+    const tokenResponse = await getNewAccessToken();
+
+    const snsWebSdkInstance = snsWebSdk
+      .init(tokenResponse, getNewAccessToken)
       .withConf({
         lang: 'en',
       })
-      .on('onError', (error) => {
-        console.log('onError', error)
+      .on('error', (error: any) => {
+        console.error('Sumsub SDK error:', error);
       })
       .onMessage(async (type: string, payload: any) => {
         try {
-          console.log('onMessage', type, type.split('.')[1], payload)
-          if (type.split('.')[1] === 'onStepCompleted' || type.split('.')[1] === 'stepCompleted') {
-            console.log('Step completed for address:', address);
+          const eventType = type.split('.')[1];
+          console.log('onMessage', type, eventType, payload);
+
+          if (eventType === 'onStepCompleted' || eventType === 'stepCompleted') {
             await axios.post(`${SUMSUB_VERIFY_URL}/verdict`, { address });
-          } else if (type.split('.')[1] === 'onApplicantStatusChanged' || payload?.reviewStatus === 'completed') {
-            console.log('Step failed for address:', address);
+          } else if (
+            eventType === 'onApplicantStatusChanged' ||
+            payload?.reviewStatus === 'completed'
+          ) {
             await axios.post(`${SUMSUB_VERIFY_URL}/verdict`, { address, stepFailed: true });
           }
         } catch (e) {
-          console.log('Error in onMessage handler:', e);
+          console.error('Error in onMessage handler:', e);
         }
       })
       .build();
-  
-    snsWebSdkInstance.launch('#sumsub-container')
+
+    snsWebSdkInstance.launch('#sumsub-container');
   }
 
   useEffect(() => {
-    launchWebSdk();
-  }, []);
+    if (address) launchWebSdk();
+  }, [address]);
 
   return <div id="sumsub-container" />;
 }
